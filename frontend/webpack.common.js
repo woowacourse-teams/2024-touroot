24c31b7f44fe8bdf37aceeee7a01bcc2e1de0b49
손진영 <sm099@naver.com>
2024-07-11 16:58:56 +0900

    chore: react 내 webpack 세팅
    1. webpack 관련 의존성 추가
    
    2. build scrpit 수정
    
    3. webpack.common.js 추가
    
    4. public 디렉터리 내 index.html 추가
    
    5. src 디렉터리 내 App.tsx 및 main.tsx 추가

---
+++ frontend/webpack.common.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  entry: './src/main.tsx',
+  output: {
+    filename: 'touroot.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    compress: true,
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  },
+};
