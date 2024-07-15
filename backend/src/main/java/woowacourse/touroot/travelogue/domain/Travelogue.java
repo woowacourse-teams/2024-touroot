f6a8fefed308ef8b3422529f4e67a3db64b1beb0
hangillee <skfcb10@naver.com>
2024-07-15 21:49:40 +0900

    feat: 여행기 관련 Entity 생성

---
+++ backend/src/main/java/woowacourse/touroot/travelogue/domain/Travelogue.java
@@ -0,0 +1,29 @@
+package woowacourse.touroot.travelogue.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.Getter;
+import woowacourse.touroot.travelogueday.domain.TravelogueDay;
+
+@Getter
+@Entity
+public class Travelogue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    @Column(nullable = false)
+    private String thumbnail;
+
+    @OneToMany(mappedBy = "travelogue")
+    private List<TravelogueDay> days;
+}
