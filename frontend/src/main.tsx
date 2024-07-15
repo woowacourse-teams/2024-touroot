0642a54e5963760a7be39b46890ebccb2be26bca
손진영 <sm099@naver.com>
2024-07-15 21:51:16 +0900

    feat: ThemeProvider 추가
    1. theme 관련 declare 파일 추가
    
    2. theme 추가
    
    3. theme을 ThemeProvider 적용

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,15 @@
 import ReactDOM from "react-dom/client";
 
-import { Global } from "@emotion/react";
+import { Global, ThemeProvider } from "@emotion/react";
 
 import { globalStyle } from "@styles/globalStyle";
+import theme from "@styles/theme";
 
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
+  <ThemeProvider theme={theme}>
     <Global styles={globalStyle} />
     <App />
-  </>,
+  </ThemeProvider>,
 );
---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,40 @@
+import { Theme } from "@emotion/react";
+
+const theme: Theme = {
+  typography: {
+    mainText: `
+      font-weight: 400;
+      font-size: 1.6rem;
+      line-height: 1.25rem;
+    `,
+    mainTextBold: `
+      font-weight: 700;
+      font-size: 1.6rem;
+      line-height: 1.25rem;
+    `,
+    title: `
+      font-weight: 700;
+      font-size: 2.4rem;
+      line-height: 2.17rem;
+    `,
+    detail: `
+      font-family: Pretendard-Regular;
+      font-weight: 400;
+      font-size: 1.2rem;
+      line-height: 1.25rem;
+    `,
+    detailBold: `
+      font-weight: 700;
+      font-size: 1.2rem;
+      line-height: 1.25rem;
+    `,
+  },
+  color: {
+    primary: "#0090ff",
+    black: "#000000",
+    darkGray: "#616161",
+    borderGray: "#d9d9d9",
+  },
+};
+
+export default theme;
