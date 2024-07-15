f8d6982e18379623af98687ecf96c195c67fef4b
Lee Hangil <skfcb10@naver.com>
2024-07-15 21:56:36 +0900

    [Feature] - 여행기 관련 Entity 생성 #17

---
+++ backend/src/main/java/woowacourse/touroot/place/domain/Place.java
@@ -0,0 +1,28 @@
+package woowacourse.touroot.place.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+    private String googlePlaceId;
+}
---
+++ backend/src/main/java/woowacourse/touroot/travelogueday/domain/TravelogueDay.java
@@ -0,0 +1,34 @@
+package woowacourse.touroot.travelogueday.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.Getter;
+import woowacourse.touroot.travelogue.domain.Travelogue;
+import woowacourse.touroot.travelogueplace.domain.TraveloguePlace;
+
+@Getter
+@Entity
+public class TravelogueDay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer order;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Travelogue travelogue;
+
+    @OneToMany(mappedBy = "travelogueDay")
+    private List<TraveloguePlace> places;
+}
