6a97cd1be9eff2869cf560a8aa24fcbe0944bba6
손진영 <sm099@naver.com>
2024-07-16 22:20:15 +0900

    feat: Header 컴포넌트 구현

---
+++ frontend/src/components/common/Header/Header.styled.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const HeaderLayout = styled.header`
+  width: 100%;
+  max-width: 48rem;
+  height: fit-content;
+  display: flex;
+  justify-content: space-between;
+  padding: 1.6rem;
+  border-bottom: 0.1rem solid #eaf4f8;
+  position: fixed;
+  top: 0;
+  background-color: #fff;
+  z-index: 1000;
+`;
