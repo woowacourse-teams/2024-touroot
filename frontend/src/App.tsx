bb731d8ba1c9481210370cb8f028a37961b40a8b
손진영 <sm099@naver.com>
2024-07-16 22:23:57 +0900

    feat: 초기 여행기 상세 페이지 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,7 @@
-export default function App() {
-  return <div>App</div>;
-}
+import TravelogueDetailPage from "@components/pages/travelogueDetail/TravelogueDetailPage";
+
+const App = () => {
+  return <TravelogueDetailPage />;
+};
+
+export default App;
