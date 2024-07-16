c2d1fc78497f49c83a09b1fdce656a10f41fc5cf
손진영 <sm099@naver.com>
2024-07-16 10:19:16 +0900

    feat: font 관련 local 함수 추가

--- frontend/src/styles/globalStyle.ts
+++ frontend/src/styles/globalStyle.ts
@@ -5,63 +5,72 @@ export const globalStyle = css`
     font-family: "Pretendard";
     font-weight: 900;
     font-display: swap;
-    src: url("@assets/fonts/woff-subset/Pretendard-Black.subset.woff") format("woff");
+    src: local("Pretendard") url("@assets/fonts/woff-subset/Pretendard-Black.subset.woff")
+      format("woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 800;
     font-display: swap;
-    src: url("@assets/fonts/woff-subset/Pretendard-ExtraBold.subset.woff") format("woff");
+    src: local("Pretendard") url("@assets/fonts/woff-subset/Pretendard-ExtraBold.subset.woff")
+      format("woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 700;
     font-display: swap;
-    src: url("@assets/fonts/woff-subset/Pretendard-Bold.subset.woff") format("woff");
+    src: local("Pretendard") url("@assets/fonts/woff-subset/Pretendard-Bold.subset.woff")
+      format("woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 600;
     font-display: swap;
-    src: url("@assets/fonts/woff-subset/Pretendard-SemiBold.subset.woff") format("woff");
+    src: local("Pretendard") url("@assets/fonts/woff-subset/Pretendard-SemiBold.subset.woff")
+      format("woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 500;
     font-display: swap;
-    src: url("@assets/fonts/woff-subset/Pretendard-Medium.subset.woff") format("woff");
+    src: local("Pretendard") url("@assets/fonts/woff-subset/Pretendard-Medium.subset.woff")
+      format("woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 400;
     font-display: swap;
-    src: url("@assets/fonts/woff-subset/Pretendard-Regular.subset.woff") format("woff");
+    src: local("Pretendard") url("/@assets/fonts/woff-subset/Pretendard-Regular.subset.woff")
+      format("woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 300;
     font-display: swap;
-    src: url("@assets/fonts/woff-subset/Pretendard-Light.subset.woff") format("woff");
+    src: local("Pretendard") url("@assets/fonts/woff-subset/Pretendard-Light.subset.woff")
+      format("woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 200;
     font-display: swap;
-    src: url("@assets/fonts/woff-subset/Pretendard-ExtraLight.subset.woff") format("woff");
+    src: local("Pretendard") url("@assets/fonts/woff-subset/Pretendard-ExtraLight.subset.woff")
+      format("woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 100;
     font-display: swap;
-    src: url("@assets/fonts/woff-subset/Pretendard-Thin.subset.woff") format("woff");
+    src: local("Pretendard") url("@assets/fonts/woff-subset/Pretendard-Thin.subset.woff")
+      format("woff");
   }
 
   html,
