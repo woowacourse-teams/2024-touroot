6d5446d8d696f225a4371c334ac350ddaed66781
손진영 <sm099@naver.com>
2024-07-19 00:06:21 +0900

    feat: TransformBottomSheet story 파일 추가

--- frontend/src/components/common/TransformBottomSheet/TransformBottomSheet.styled.ts
+++ frontend/src/components/common/TransformBottomSheet/TransformBottomSheet.styled.ts
@@ -20,27 +20,29 @@ const slideDown = keyframes`
 `;
 
 export const BottomSheetLayout = styled.div<{ $isOpen: boolean }>`
+  display: flex;
+  overflow: hidden;
   position: fixed;
   bottom: 0;
+  z-index: 1000;
   width: 100%;
+  height: ${({ $isOpen }) => ($isOpen ? "12rem" : "5rem")};
+
+  background-color: white;
+
+  animation: ${({ $isOpen }) => ($isOpen ? slideUp : slideDown)} 0.3s ease-out;
   max-width: 48rem;
-  display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: ${({ $isOpen }) => ($isOpen ? "12rem" : "5rem")};
-  background-color: white;
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
-  box-shadow: 0px -2px 10px rgba(0, 0, 0, 0.1);
-  animation: ${({ $isOpen }) => ($isOpen ? slideUp : slideDown)} 0.3s ease-out;
-  overflow: hidden;
-  z-index: 1000;
+  box-shadow: 0 -2px 10px rgb(0 0 0 / 10%);
 `;
 
 export const BottomSheetContent = styled.div`
-  padding: 20px;
   height: 100%;
+  padding: 20px;
   overflow-y: auto;
 
   ${(props) => props.theme.typography.detailBold}
@@ -52,17 +54,20 @@ export const BottomSheetBottomContainer = styled.div`
   left: 0;
   width: 100%;
   padding: 10px;
+
   background-color: white;
 `;
 
 export const BottomSheetButton = styled.button`
   width: 100%;
   padding: 10px;
-  background-color: #007bff;
-  color: white;
   border: none;
-  border-radius: 5px;
-  font-size: 16px;
+
+  background-color: #007bff;
+
+  color: white;
   font-weight: bold;
+  font-size: 16px;
+  border-radius: 5px;
   cursor: pointer;
 `;
