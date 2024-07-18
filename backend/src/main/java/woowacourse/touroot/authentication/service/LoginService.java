b0a0b00d46f2d660c15e03f1207c074ff4973dfe
nhlee98 <e5slnh9876@gmail.com>
2024-07-18 16:00:39 +0900

    feat: 소셜 로그인 회원가입 분기 흐름 구현

--- backend/src/main/java/woowacourse/touroot/authentication/service/LoginService.java
+++ backend/src/main/java/woowacourse/touroot/authentication/service/LoginService.java
@@ -1,16 +1,31 @@
 package woowacourse.touroot.authentication.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import woowacourse.touroot.authentication.dto.OauthUserInformationResponse;
 import woowacourse.touroot.authentication.infrastructure.KakaoOauthProvider;
+import woowacourse.touroot.member.domain.Member;
+import woowacourse.touroot.member.repository.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
 
+    private final MemberRepository memberRepository;
     private final KakaoOauthProvider oauthProvider;
 
     public String login(String code) {
-        return oauthProvider.getUserInformation(code).profileImage();
+        OauthUserInformationResponse userInformation = oauthProvider.getUserInformation(code);
+        Member member = memberRepository.findByKakaoId(userInformation.socialLoginId())
+                .orElseGet(() -> signUp(userInformation));
+
+        return userInformation.profileImage();
+    }
+
+    private Member signUp(OauthUserInformationResponse userInformation) {
+        return memberRepository.save(
+                new Member(userInformation.socialLoginId(), userInformation.nickname(), userInformation.profileImage())
+        );
     }
 }
