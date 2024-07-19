c830b7634706ee5992b07c816ba0db3c7a06bba9
eunjungL <dldmswjd322@gmail.com>
2024-07-19 15:07:29 +0900

    feat: MethodArgumentNotValidException handler 추가

--- backend/src/main/java/woowacourse/touroot/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/woowacourse/touroot/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package woowacourse.touroot.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import woowacourse.touroot.global.exception.dto.ExceptionResponse;
@@ -13,7 +14,23 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ExceptionResponse> handleBadRequestException(BadRequestException exception) {
         log.info("BAD_REQUEST_EXCEPTION :: message = {}", exception.getMessage());
+
         ExceptionResponse data = new ExceptionResponse(exception.getMessage());
+        return ResponseEntity.badRequest()
+                .body(data);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception
+    ) {
+        log.info("METHOD_ARGUMENT_NOT_VALID_EXCEPTION :: message = {}", exception.getMessage());
+
+        String message = exception.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+        ExceptionResponse data = new ExceptionResponse(message);
         return ResponseEntity.badRequest()
                 .body(data);
     }
