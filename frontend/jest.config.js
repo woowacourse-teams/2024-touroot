8f27f95880fdd5f519f73d71f62458ebd98101c8
손진영 <sm099@naver.com>
2024-07-19 15:25:04 +0900

    chore: jest.config.js 내 reporters 옵션 추가

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -11,4 +11,14 @@ module.exports = {
     "^@apis/(.*)$": "<rootDir>/src/apis/$1",
   },
   testPathIgnorePatterns: ["<rootDir>/__tests__/constants/", "<rootDir>/__tests__/utils/"],
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "test-results",
+        outputName: "results.xml",
+      },
+    ],
+  ],
 };
