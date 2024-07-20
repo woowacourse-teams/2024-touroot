c878db783d77f8a2e4e824390c4dfc1676931cae
손진영 <sm099@naver.com>
2024-07-20 13:13:35 +0900

    chore: @mocks 관련 설정 추가
    1. path alias 추가(webpack, tsconfig)
    
    2. prettier import sort 내 mocks 추가

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -18,6 +18,7 @@ module.exports = {
       "@assets": path.resolve(__dirname, "src/assets/"),
       "@components": path.resolve(__dirname, "src/components/"),
       "@apis": path.resolve(__dirname, "src/apis/"),
+      "@mocks": path.resolve(__dirname, "src/mocks/"),
     },
     extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
