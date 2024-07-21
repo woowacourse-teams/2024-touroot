99d0c0aba73b74f5da6b52db53d2c52874ff1279
river <130737187+0jenn0@users.noreply.github.com>
2024-07-21 19:56:08 +0900

    feat: AvatarCircle 컴포넌트 구현

---
+++ frontend/src/assets/svg/index.ts
@@ -0,0 +1,10 @@
+export { default as Hamburger } from "./hamburger.svg";
+export { default as BackIcon } from "./back-icon.svg";
+export { default as NextArrow } from "./next-arrow.svg";
+export { default as PrevArrow } from "./prev-arrow.svg";
+export { default as UpArrow } from "./up-arrow.svg";
+export { default as DownArrow } from "./down-arrow.svg";
+export { default as Marker } from "./marker.svg";
+export { default as markerUrl } from "./marker.svg?url";
+export { default as EmptyHeart } from "./empty-heart.svg";
+export { default as DefaultAvatar } from "./default-avatar.svg";
