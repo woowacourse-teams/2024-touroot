3ce15e5cb52873fbcf1f04977fee19b484e7d6e9
simhorim <hori5020@gmail.com>
2024-07-21 23:21:26 +0900

    feat(LoginPage): 카카오 로그인 구현

--- frontend/src/components/pages/login/LoginPage.tsx
+++ frontend/src/components/pages/login/LoginPage.tsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
+
+import { AxiosResponse } from "axios";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { client } from "@apis/client";
+
 import { Header } from "@components/common";
 
 import { BackIcon, KakaoSymbol } from "@assets/svg";
 import { Tturi } from "@assets/webp";
 
 import * as S from "./LoginPage.styled";
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+interface KakaoLoginResult {
+  accessToken: "string";
+}
+
+const kakao = window.Kakao;
 
 const LoginPage = () => {
   const LOGIN_TEXT = "로그인";
@@ -12,10 +31,33 @@ const LoginPage = () => {
   const GREETING_SUB_TEXT = "To your route, touroot!";
   const KAKAO_LABEL = "카카오 로그인";
 
+  // TODO: 이전 화면으로 돌아가기 추가
+  const handleClickBackIcon = () => {};
+
+  const handleClickLoginButton = () => {
+    kakao?.Auth?.authorize({
+      redirectUri: process.env.REDIRECT_URI,
+    });
+
+    // TODO: 백엔드 api 명세서에 따라 수정 예정
+    // const { data } = useQuery<AxiosResponse<KakaoLoginResult>>({
+    //   queryKey: ["login"],
+    //   queryFn: () => client.get("/api/v1/login/oauth/kakao"),
+    // });
+  };
+
+  useEffect(() => {
+    if (!kakao?.isInitialized()) {
+      kakao?.init(process.env.JAVASCRIPT_KEY);
+    }
+  }, []);
+
   return (
     <>
       <Header>
-        <BackIcon />
+        <button onClick={handleClickBackIcon}>
+          <BackIcon />
+        </button>
         <S.LoginText>{LOGIN_TEXT}</S.LoginText>
         <S.HiddenDiv />
       </Header>
@@ -27,7 +69,7 @@ const LoginPage = () => {
         </S.GreetingBox>
       </S.GreetingContainer>
       <S.LoginButtonWrapper>
-        <S.LoginButton>
+        <S.LoginButton onClick={handleClickLoginButton}>
           <KakaoSymbol />
           <S.LoginLabel>{KAKAO_LABEL}</S.LoginLabel>
         </S.LoginButton>
