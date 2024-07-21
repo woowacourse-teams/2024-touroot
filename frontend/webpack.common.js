b3a64239d796bb0fbdb09e8baa7780a4b7730971
손진영 <sm099@naver.com>
2024-07-22 01:27:57 +0900

    chore(webpack.common.js): publicPath 추가

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: "touroot.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    // publicPath: "/",
+    //TODO: publicPath: "/",
   },
   resolve: {
     alias: {
