d98379d9cbd80edd6b9393402c0edb41a50a2f57
eunjungL <dldmswjd322@gmail.com>
2024-07-21 22:22:42 +0900

    feat: UnauthorizedException 추가

---
+++ backend/src/main/java/woowacourse/touroot/global/exception/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package woowacourse.touroot.global.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
