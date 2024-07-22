eb9f1838424fb130ebd9d40a923c1d9b25b07a74
river <130737187+0jenn0@users.noreply.github.com>
2024-07-22 22:28:17 +0900

    test: MultiImageUpload 컴포넌트 storybook 추가

---
+++ frontend/src/components/common/MultiImageUpload/MultiImageUpload.stories.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+import MultiImageUpload from "./MultiImageUpload";
+
+const meta = {
+  title: "common/ThumbnailUpload",
+  component: MultiImageUpload,
+  decorators: [
+    (Story, context) => {
+      return (
+        <div style={{ width: "48rem" }}>
+          <Story args={{ ...context.args }} />
+        </div>
+      );
+    },
+  ],
+} satisfies Meta<typeof MultiImageUpload>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const WithImage: Story = {
+  decorators: [
+    (Story) => {
+      React.useEffect(() => {
+        const fetchAndCreateFile = async () => {
+          const imageUrl =
+            "https://api.allorigins.win/raw?url=https://i.pinimg.com/474x/df/6d/1a/df6d1a665685af3c0eb7e4c6a0c40169.jpg";
+          const file = new File([await fetch(imageUrl).then((r) => r.blob())], "example.png", {
+            type: "image/png",
+          });
+
+          const files = Array(5).fill(file);
+
+          const dataTransfer = new DataTransfer();
+
+          files.map((file) => dataTransfer.items.add(file));
+
+          const inputElement = document.querySelector('input[type="file"]') as HTMLInputElement;
+          if (inputElement) {
+            Object.defineProperty(inputElement, "files", {
+              value: dataTransfer.files,
+            });
+
+            const event = new Event("change", { bubbles: true });
+            inputElement.dispatchEvent(event);
+          }
+        };
+
+        fetchAndCreateFile();
+      }, []);
+
+      return <Story />;
+    },
+  ],
+};
