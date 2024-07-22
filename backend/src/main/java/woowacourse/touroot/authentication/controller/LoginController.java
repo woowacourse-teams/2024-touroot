a99c34140d0531d0564ab2e50e36830e670688b6
리비 <Leekh10195@gmail.com>
2024-07-22 15:26:20 +0900

    [Feature] - 인증과정 예외처리 (#84)
    * feat: 로그인 응답에 데이터 추가, Response 생성 방식 캡슐화
    
    - 카카오 닉네임, 프로필 이미지 경로
    
    * refactor: RestClient 예외 처리 작성
    
    * feat: 서버 내부 에러 처리 핸들러 작성
    
    * feat: Swagger api 정보 추가
    
    * fix: 최상위 예외 처리 코드 제거 개선

--- backend/src/main/java/woowacourse/touroot/authentication/controller/LoginController.java
+++ backend/src/main/java/woowacourse/touroot/authentication/controller/LoginController.java
@@ -1,5 +1,10 @@
 package woowacourse.touroot.authentication.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import woowacourse.touroot.authentication.dto.LoginResponse;
 import woowacourse.touroot.authentication.service.LoginService;
+import woowacourse.touroot.global.exception.dto.ExceptionResponse;
 
+@Tag(name = "로그인")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/login")
@@ -16,6 +23,16 @@ public class LoginController {
 
     private final LoginService loginService;
 
+    @Operation(
+            summary = "카카오 로그인",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "유효하지 않은 인가 코드로 로그인 요청을 했을 때",
+                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
+                    )
+            }
+    )
     @GetMapping("/oauth/kakao")
     public ResponseEntity<LoginResponse> login(@RequestParam(name = "code") String authorizationCode) {
         return ResponseEntity.ok()
