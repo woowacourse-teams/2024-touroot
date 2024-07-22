4c099c05f64223eab367beae705d3d4f45c59c8f
river <130737187+0jenn0@users.noreply.github.com>
2024-07-22 23:02:19 +0900

    test: MultiImageUpload 컴포넌트 storybook에 이미지 많이 첨부한 경우 추가

--- frontend/src/components/common/MultiImageUpload/MultiImageUpload.stories.tsx
+++ frontend/src/components/common/MultiImageUpload/MultiImageUpload.stories.tsx
@@ -24,7 +24,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
 
-export const WithImage: Story = {
+export const WithImages: Story = {
   decorators: [
     (Story) => {
       React.useEffect(() => {
@@ -35,7 +35,43 @@ export const WithImage: Story = {
             type: "image/png",
           });
 
-          const files = Array(5).fill(file);
+          const files = Array(3).fill(file);
+
+          const dataTransfer = new DataTransfer();
+
+          files.map((file) => dataTransfer.items.add(file));
+
+          const inputElement = document.querySelector('input[type="file"]') as HTMLInputElement;
+          if (inputElement) {
+            Object.defineProperty(inputElement, "files", {
+              value: dataTransfer.files,
+            });
+
+            const event = new Event("change", { bubbles: true });
+            inputElement.dispatchEvent(event);
+          }
+        };
+
+        fetchAndCreateFile();
+      }, []);
+
+      return <Story />;
+    },
+  ],
+};
+
+export const WithManyImages: Story = {
+  decorators: [
+    (Story) => {
+      React.useEffect(() => {
+        const fetchAndCreateFile = async () => {
+          const imageUrl =
+            "https://api.allorigins.win/raw?url=https://i.pinimg.com/474x/df/6d/1a/df6d1a665685af3c0eb7e4c6a0c40169.jpg";
+          const file = new File([await fetch(imageUrl).then((r) => r.blob())], "example.png", {
+            type: "image/png",
+          });
+
+          const files = Array(7).fill(file);
 
           const dataTransfer = new DataTransfer();
 
