97695a559998884576e18afa75cc85c5f9009b60
손진영 <sm099@naver.com>
2024-07-23 19:56:00 +0900

    refactor(Text): 변수 내에서 export 하도록 변경

--- frontend/src/components/common/Text/Text.styled.ts
+++ frontend/src/components/common/Text/Text.styled.ts
@@ -2,8 +2,6 @@ import styled from "@emotion/styled";
 
 import type { TextProps } from "./Text";
 
-const Text = styled.p<{ $textType: TextProps["textType"] }>`
+export const Text = styled.p<{ $textType: TextProps["textType"] }>`
   ${({ theme, $textType = "body" }) => theme.typography.mobile[$textType]}
 `;
-
-export { Text };
