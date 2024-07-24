5f81d915568285a5d4ee15d8b0704fab78854f9d
libienz <leekh10195@gmail.com>
2024-07-24 17:44:03 +0900

    test: LoginService Mock Test 작성

---
+++ backend/src/test/java/woowacourse/touroot/authentication/fixture/MemberFixture.java
@@ -0,0 +1,8 @@
+package woowacourse.touroot.authentication.fixture;
+
+import woowacourse.touroot.member.domain.Member;
+
+public class MemberFixture {
+
+    public static final Member MEMBER_1 = new Member(1L, 1L, "리비", "image-url");
+}
