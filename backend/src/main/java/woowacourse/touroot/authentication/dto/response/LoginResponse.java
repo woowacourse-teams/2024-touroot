708ad71bb584803911089f9cc3107b5651f6c8a0
libienz <leekh10195@gmail.com>
2024-07-24 19:52:32 +0900

    fix: api 명세 수정

--- backend/src/main/java/woowacourse/touroot/authentication/dto/response/LoginResponse.java
+++ backend/src/main/java/woowacourse/touroot/authentication/dto/response/LoginResponse.java
@@ -5,7 +5,7 @@ import woowacourse.touroot.member.domain.Member;
 
 public record LoginResponse(
         @Schema(description = "로그인된 유저의 닉네임", example = "리비") String nickname,
-        @Schema(description = "로그인된 유저의 프로필 이미지 경로", example = "https://s3-object-url") String profileImageUrl,
+        @Schema(description = "로그인된 유저의 프로필 이미지 경로", example = "https://img-ul") String profileImageUrl,
         @Schema(description = "투룻 서비스 인가용 accessToken", example = "accessTokenValue") String accessToken) {
 
     public static LoginResponse of(Member member, String accessToken) {
