0e0ccd0a90b3ddd00862606ae6ffa51a83aaa112
손진영 <sm099@naver.com>
2024-07-24 20:54:48 +0900

    refactor(ThumbnailUpload): useImageUpload 반환 값을 외부에서 주입 받도록 변경

--- frontend/src/components/common/ThumbnailUpload/ThumbnailUpload.tsx
+++ frontend/src/components/common/ThumbnailUpload/ThumbnailUpload.tsx
@@ -1,26 +1,35 @@
 import { useState } from "react";
 
-import { useImageUpload } from "@components/hooks/useImageUpload";
-
 import { PictureIcon } from "@assets/svg";
 
 import * as S from "./ThumbnailUpload.styled";
 
-const ThumbnailUpload = () => {
+interface ThumbnailUploadProps {
+  previewUrls: string[];
+  fileInputRef: React.RefObject<HTMLInputElement>;
+  onChangeImage: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onClickButton: () => void;
+}
+
+const ThumbnailUpload = ({
+  previewUrls,
+  fileInputRef,
+  onChangeImage,
+  onClickButton,
+}: ThumbnailUploadProps) => {
   const [isShowEditButton, setIsShowEditButton] = useState<boolean>(false);
-  const { images, fileInputRef, handleImageChange, handleButtonClick } = useImageUpload();
 
   const handleMouseOver = () => setIsShowEditButton(true);
   const handleMouseLeave = () => setIsShowEditButton(false);
 
-  const image = images[0];
+  const image = previewUrls[0];
 
   const HiddenInput = (
     <S.ThumbnailUploadHiddenInput
       ref={fileInputRef}
       type="file"
       accept="image/*"
-      onChange={handleImageChange}
+      onChange={onChangeImage}
       aria-label="썸네일 이미지 선택"
       title="이미지 파일을 선택하세요"
     />
@@ -30,11 +39,7 @@ const ThumbnailUpload = () => {
     <S.ThumbnailUploadContainer>
       {!image ? (
         <>
-          <S.ThumbnailUploadButton
-            type="button"
-            onClick={handleButtonClick}
-            aria-label="이미지 업로드"
-          >
+          <S.ThumbnailUploadButton type="button" onClick={onClickButton} aria-label="이미지 업로드">
             <PictureIcon />
             <p>썸네일 업로드</p>
           </S.ThumbnailUploadButton>
@@ -46,7 +51,7 @@ const ThumbnailUpload = () => {
           onMouseLeave={handleMouseLeave}
         >
           {isShowEditButton && (
-            <S.ThumbnailUploadEditButton onClick={handleButtonClick}>
+            <S.ThumbnailUploadEditButton onClick={onClickButton}>
               썸네일 수정하기
             </S.ThumbnailUploadEditButton>
           )}
