2d73e61d44c4437d027621a07531ba6316f2a855
eunjungL <62099953+eunjungL@users.noreply.github.com>
2024-07-24 18:10:19 +0900

    [Feature] - FE develop CORS 처리 (#108)

--- backend/src/main/java/woowacourse/touroot/global/config/WebConfig.java
+++ backend/src/main/java/woowacourse/touroot/global/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://dev.touroot.kr");
     }
 }
