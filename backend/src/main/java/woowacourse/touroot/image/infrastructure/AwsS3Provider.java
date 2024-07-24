5a95ba1c79430848588a6188dbd71a8aa8637a60
최휘용 <99064014+slimsha2dy@users.noreply.github.com>
2024-07-24 17:46:07 +0900

    [Feature] - AWS S3 이미지 저장 구현 (#98)
    * chore: aws s3 sdk 의존성 추가
    
    * feat: MultipartFile을 S3에 업로드하는 기능 구현
    
    * feat: 이미지를 저장하는 Controller, Service 구현
    
    * style: 불필요한 어노테이션 제거
    
    * feat: 이미지 파일 확장자 검증 로직 추가
    
    * feat: Swagger 기본 코드 추가
    
    * feat: s3 이미지 저장 경로 변경
    
    * feat: Swagger 예외 명세 추가
    
    * feat: 확장자 관련 예외 처리 추가
    
    * style: createFileName 메서드명 createFilePath로 변경
    
    * style: Swagger 어노테이션 분리
    
    * style: 메서드 체이닝 개행 추가
    
    * refactor: MultipartFile을 ImageFile로 포장 및 검증 로직 위치 변경
    
    - AwsS3Provider가 upload의 매개변수로 MultipartFile이 아닌 ImageFile을 받게됨으로써 메서드명도 수정함

---
+++ backend/src/main/java/woowacourse/touroot/image/infrastructure/AwsS3Provider.java
@@ -0,0 +1,78 @@
+package woowacourse.touroot.image.infrastructure;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import woowacourse.touroot.global.exception.BadRequestException;
+import woowacourse.touroot.image.domain.ImageFile;
+
+@Component
+public class AwsS3Provider {
+
+    private final String bucket;
+    private final String directoryPath;
+
+    public AwsS3Provider(
+            @Value("${cloud.aws.s3.bucket}") String bucket,
+            @Value("${cloud.aws.s3.directory-path}") String directoryPath
+    ) {
+        this.bucket = bucket;
+        this.directoryPath = directoryPath;
+    }
+
+    public List<String> uploadImages(List<ImageFile> files) {
+        List<String> urls = new ArrayList<>();
+
+        try (S3Client s3Client = getS3Client()) {
+            files.stream()
+                    .map(ImageFile::getFile)
+                    .forEach(file -> {
+                        String filePath = createFilePath(file.getOriginalFilename());
+                        uploadFile(file, filePath, s3Client);
+                        urls.add(getFileUrl(filePath, s3Client));
+                    });
+            return urls;
+        }
+    }
+
+    private S3Client getS3Client() {
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(InstanceProfileCredentialsProvider.create())
+                .build();
+    }
+
+    private void uploadFile(MultipartFile file, String filePath, S3Client s3Client) {
+        try {
+            RequestBody requestBody = RequestBody.fromBytes(file.getBytes());
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(createFilePath(filePath))
+                    .build();
+
+            s3Client.putObject(putObjectRequest, requestBody);
+        } catch (IOException e) {
+            throw new BadRequestException("파일 저장에 실패했습니다.");
+        }
+    }
+
+    private String getFileUrl(String filePath, S3Client s3Client) {
+        return s3Client.utilities()
+                .getUrl(builder -> builder.bucket(bucket)
+                        .key(filePath))
+                .toString();
+    }
+
+    private String createFilePath(String fileName) {
+        return directoryPath + UUID.randomUUID() + fileName.substring(fileName.lastIndexOf("."));
+    }
+}
