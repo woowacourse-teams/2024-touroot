65a0c9f5d7c324e4e5cef9f7ba5d0387038bc469
손진영 <sm099@naver.com>
2024-07-24 21:02:08 +0900

    feat: 여행기 등록 기능 구현
    1. router에 추가
    
    2. 여행기 등록 페이지 추가

---
+++ frontend/src/components/pages/travelogueRegister/TravelogueRegisterPage.tsx
@@ -0,0 +1,158 @@
+import { useState } from "react";
+
+import { css } from "@emotion/react";
+
+import { usePostTravelogue, usePostUploadImages } from "@queries/index";
+
+import {
+  Accordion,
+  Button,
+  DayContent,
+  GoogleMapLoadScript,
+  IconButton,
+  Input,
+  ModalBottomSheet,
+  PageInfo,
+  Text,
+  ThumbnailUpload,
+} from "@components/common";
+import TravelogueMultiImageUpload from "@components/pages/travelogueRegister/TravelogueMultiImageUpload/TravelogueMultiImageUpload";
+
+import { useImageUpload } from "@hooks/index";
+import { useTravelDays } from "@hooks/pages/useTravelDays";
+
+import * as S from "./TravelogueRegisterPage.styled";
+
+const MAX_TITLE_LENGTH = 20;
+
+const TravelogueRegisterPage = () => {
+  const [title, setTitle] = useState("");
+  const [thumbnail, setThumbnail] = useState("");
+
+  const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const {
+    travelDays,
+    onAddDay,
+    onAddPlace,
+    onDeleteDay,
+    onChangePlaceDescription,
+    onDeletePlace,
+    onChangeImageUrls,
+    onDeleteImageUrls,
+  } = useTravelDays();
+
+  const {
+    previewUrls: thumbnailPreviewUrls,
+    fileInputRef: thumbnailFileInputRef,
+    handleImageChange: handleThumbnailImageChange,
+    handleButtonClick: handleThumbnailButtonClick,
+  } = useImageUpload();
+
+  const handleChangeThumbnail = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    handleThumbnailImageChange(e);
+    const thumbnail = await handleAddImage(Array.from(e.target.files as FileList));
+    setThumbnail(thumbnail[0]);
+  };
+
+  const { mutateAsync: handleAddImage } = usePostUploadImages();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenBottomSheet = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseBottomSheet = () => {
+    setIsOpen(false);
+  };
+
+  const handleConfirmBottomSheet = () => {
+    handleRegisterTravelogue({ title, thumbnail, days: travelDays });
+    handleCloseBottomSheet();
+  };
+
+  const { mutate: handleRegisterTravelogue } = usePostTravelogue();
+
+  return (
+    <>
+      <S.Layout>
+        <PageInfo mainText="여행기 등록" subText="소중한 여행기를 공유해주세요." />
+        <Input
+          value={title}
+          maxLength={MAX_TITLE_LENGTH}
+          label="제목"
+          count={title.length}
+          maxCount={MAX_TITLE_LENGTH}
+          onChange={handleChangeTitle}
+        />
+        <S.PageInfoContainer>
+          <Text
+            css={css`
+              font-weight: 700;
+            `}
+            textType="body"
+          >
+            썸네일
+          </Text>
+          <ThumbnailUpload
+            previewUrls={thumbnailPreviewUrls}
+            fileInputRef={thumbnailFileInputRef}
+            onChangeImage={handleChangeThumbnail}
+            onClickButton={handleThumbnailButtonClick}
+          />
+        </S.PageInfoContainer>
+        <S.AccordionRootContainer>
+          <GoogleMapLoadScript libraries={["places", "maps"]}>
+            <Accordion.Root>
+              {travelDays.map((travelDay, dayIndex) => (
+                <DayContent
+                  key={`${travelDay}-${dayIndex}`}
+                  travelDay={travelDay}
+                  dayIndex={dayIndex}
+                  onAddPlace={onAddPlace}
+                  onDeletePlace={onDeletePlace}
+                  onDeleteDay={onDeleteDay}
+                  onChangePlaceDescription={onChangePlaceDescription}
+                >
+                  {(placeIndex) => (
+                    <TravelogueMultiImageUpload
+                      dayIndex={dayIndex}
+                      placeIndex={placeIndex}
+                      onChangeImageUrls={onChangeImageUrls}
+                      onDeleteImageUrls={onDeleteImageUrls}
+                      onRequestAddImage={handleAddImage}
+                    />
+                  )}
+                </DayContent>
+              ))}
+            </Accordion.Root>
+            <IconButton
+              size="16"
+              iconType="plus"
+              position="left"
+              css={[S.addButtonStyle, S.addDayButtonStyle]}
+              onClick={onAddDay}
+            >
+              일자 추가하기
+            </IconButton>
+          </GoogleMapLoadScript>
+          <Button variants="primary" onClick={handleOpenBottomSheet}>
+            등록
+          </Button>
+        </S.AccordionRootContainer>
+      </S.Layout>
+      <ModalBottomSheet
+        isOpen={isOpen}
+        mainText="여행기를 등록할까요?"
+        subText="등록한 후에도 다시 여행기를 수정할 수 있어요!"
+        onClose={handleCloseBottomSheet}
+        onConfirm={handleConfirmBottomSheet}
+      />
+    </>
+  );
+};
+
+export default TravelogueRegisterPage;
