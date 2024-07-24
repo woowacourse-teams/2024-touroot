780ae1bb280e74c5dd5846d79eb2b9aa94082a7e
river <130737187+0jenn0@users.noreply.github.com>
2024-07-24 23:56:18 +0900

    feat: useInfiniteTravelogues 구현
    - 5개씩 fetch해옵니다.

---
+++ frontend/src/queries/useInfiniteTravelogues.ts
@@ -0,0 +1,47 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { client } from "@apis/client";
+
+export const getTravelogues = async ({ page, size }: { page: number; size: number }) => {
+  try {
+    const response = await client.get("/api/v1/travelogues", {
+      params: { page, size },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching travelogues:", error);
+    throw error;
+  }
+};
+
+const useInfiniteTravelogues = () => {
+  const INITIAL_PAGE = 0;
+  const DATA_LOAD_COUNT = 5;
+
+  const { data, status, error, fetchNextPage, isFetchingNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ["travelogues"],
+    queryFn: ({ pageParam = INITIAL_PAGE }) => {
+      const page = pageParam;
+      const size = DATA_LOAD_COUNT;
+      console.log({ page, size });
+      return getTravelogues({ page, size });
+    },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      const nextPage = lastPage.content.length ? allPages.length : undefined;
+
+      return nextPage;
+    },
+  });
+
+  return {
+    travelogues: data?.pages.flatMap((page) => page.content) || [],
+    status,
+    error,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  };
+};
+
+export default useInfiniteTravelogues;
