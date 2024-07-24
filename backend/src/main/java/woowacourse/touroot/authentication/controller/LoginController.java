16ec7b8f0478ad80d906a9062c79c45dd706dc19
libienz <leekh10195@gmail.com>
2024-07-24 13:52:16 +0900

    refactor: example과 response 추가를 통한 api 명세 구체화 개선

--- backend/src/main/java/woowacourse/touroot/authentication/controller/LoginController.java
+++ backend/src/main/java/woowacourse/touroot/authentication/controller/LoginController.java
@@ -1,9 +1,11 @@
 package woowacourse.touroot.authentication.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,18 +25,23 @@ public class LoginController {
 
     private final LoginService loginService;
 
-    @Operation(
-            summary = "카카오 로그인",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "400",
-                            description = "유효하지 않은 인가 코드로 로그인 요청을 했을 때",
-                            content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
-                    )
-            }
-    )
+    @Operation(summary = "카카오 소셜 로그인")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "요청이 정상적으로 처리되었을 때"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "요청 파라미터에 올바르지 않은 인가코드값이 전달되었을 때",
+                    content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
+            )
+    })
     @GetMapping("/oauth/kakao")
-    public ResponseEntity<LoginResponse> login(@RequestParam(name = "code") String authorizationCode) {
+    public ResponseEntity<LoginResponse> login(
+            @Parameter(description = "인가 코드") @RequestParam(name = "code") String authorizationCode
+    ) {
+
         return ResponseEntity.ok()
                 .body(loginService.login(authorizationCode));
     }
--- backend/src/main/java/woowacourse/touroot/authentication/dto/LoginResponse.java
+++ backend/src/main/java/woowacourse/touroot/authentication/dto/LoginResponse.java
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import woowacourse.touroot.member.domain.Member;
 
 public record LoginResponse(
-        @Schema(description = "로그인된 유저의 닉네임") String nickname,
-        @Schema(description = "로그인된 유저의 프로필 이미지 경로") String profileImageUrl,
-        @Schema(description = "인가에 필요한 accessToken") String accessToken) {
+        @Schema(description = "로그인된 유저의 닉네임", example = "리비") String nickname,
+        @Schema(description = "로그인된 유저의 프로필 이미지 경로", example = "https://s3-object-url") String profileImageUrl,
+        @Schema(description = "투룻 서비스 인가용 accessToken", example = "accessTokenValue") String accessToken) {
 
     public static LoginResponse of(Member member, String accessToken) {
         return new LoginResponse(member.getNickname(), member.getProfileImageUri(), accessToken);
