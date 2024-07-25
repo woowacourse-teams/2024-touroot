7d31b3b0bebb990052129bf4f3009dc514b7cbd1
simhorim <hori5020@gmail.com>
2024-07-25 11:38:32 +0900

    feat(App): useContext 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,48 @@
+import { createContext, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "./router";
 
+export interface User {
+  accessToken: string;
+  nickname: string;
+  profileImageUrl: string;
+}
+
+export const useUser = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const saveUser = (userInfo: User) => {
+    setUser(userInfo);
+  };
+
+  return {
+    user,
+    saveUser,
+  };
+};
+
+interface UseUserContextProps {
+  user: User | null;
+}
+
+interface UseSetUserContextProps {
+  saveUser: (userInfo: User) => void;
+}
+
+export const UseUserContext = createContext({} as UseUserContextProps);
+export const UseSetUserContext = createContext({} as UseSetUserContextProps);
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  const { user, saveUser } = useUser();
+
+  return (
+    <UseUserContext.Provider value={{ user }}>
+      <UseSetUserContext.Provider value={{ saveUser }}>
+        <RouterProvider router={router} />
+      </UseSetUserContext.Provider>
+    </UseUserContext.Provider>
+  );
 };
 
 export default App;
