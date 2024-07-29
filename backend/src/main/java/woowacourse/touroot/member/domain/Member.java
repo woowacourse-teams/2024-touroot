2db33ec694c73ac400b86881fd73b11c3c6656a5
libienz <leekh10195@gmail.com>
2024-07-29 21:43:51 +0900

    feat: 멤버 검증 로직 추가
    - 필드 not null
    - 문자열 필드 not blank

--- backend/src/main/java/woowacourse/touroot/member/domain/Member.java
+++ backend/src/main/java/woowacourse/touroot/member/domain/Member.java
@@ -6,13 +6,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import woowacourse.touroot.global.entity.BaseEntity;
+import woowacourse.touroot.global.exception.BadRequestException;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PUBLIC)
 @Getter
 @Entity
 public class Member extends BaseEntity {
@@ -30,7 +29,32 @@ public class Member extends BaseEntity {
     @Column(nullable = false)
     private String profileImageUri;
 
+    public Member(Long id, Long kakaoId, String nickname, String profileImageUri) {
+        validate(kakaoId, nickname, profileImageUri);
+        this.id = id;
+        this.kakaoId = kakaoId;
+        this.nickname = nickname;
+        this.profileImageUri = profileImageUri;
+    }
+
     public Member(Long kakaoId, String nickname, String profileImageUri) {
         this(null, kakaoId, nickname, profileImageUri);
+    }
+
+    private void validate(Long kakaoId, String nickname, String profileImageUri) {
+        validateNotNull(kakaoId, nickname, profileImageUri);
+        validateNotBlank(nickname, profileImageUri);
+    }
+
+    private void validateNotNull(Long kakaoId, String nickname, String profileImageUri) {
+        if (kakaoId == null || nickname == null || profileImageUri == null) {
+            throw new BadRequestException("카카오 아이디, 닉네임, 프로필 이미지는 비어 있을 수 없습니다");
+        }
+    }
+
+    private void validateNotBlank(String nickname, String profileImageUri) {
+        if (nickname.isBlank() || profileImageUri.isBlank()) {
+            throw new BadRequestException("닉네임, 프로필 이미지는 비어있을 수 없습니다");
+        }
     }
 }
