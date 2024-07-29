edee6cb2418a82e209ad9b772356afff55cbfb25
손진영 <sm099@naver.com>
2024-07-29 23:38:08 +0900

    fix(useGetTravelogue): 반환하는 데이터 형태가 달라 런타임 에러 발생 해결
    data return 형태 변경

--- frontend/src/queries/useGetTravelogue.ts
+++ frontend/src/queries/useGetTravelogue.ts
@@ -2,12 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 
 import { Travelogue } from "@type/domain/travelogue";
 
-
 import { client } from "@apis/client";
 
 export const useGetTravelogue = (id: string) => {
   return useQuery<Travelogue>({
     queryKey: [`travelogues/${id}`],
-    queryFn: () => client.get(`/travelogues/${id}`),
+    queryFn: async () => {
+      const { data } = await client.get(`/travelogues/${id}`);
+
+      return data;
+    },
   });
 };
