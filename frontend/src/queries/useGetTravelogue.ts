950455602f6e75090a80ee2eb1cec54f5f50bf14
손진영 <sm099@naver.com>
2024-07-29 21:00:49 +0900

    fix(useGetTravelogue): merge 과정에서 queryFn 사라진 오류 수정

--- frontend/src/queries/useGetTravelogue.ts
+++ frontend/src/queries/useGetTravelogue.ts
@@ -2,11 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 
 import { Travelogue } from "@type/domain/travelogue";
 
-import { getTravelogue } from "@apis/travelogue";
+
+import { client } from "@apis/client";
 
 export const useGetTravelogue = (id: string) => {
   return useQuery<Travelogue>({
     queryKey: [`travelogues/${id}`],
-    queryFn: () => getTravelogue(id),
+    queryFn: () => client.get(`/travelogues/${id}`),
   });
 };
