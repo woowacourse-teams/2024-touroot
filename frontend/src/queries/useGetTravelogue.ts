c148e0b67a77bba4d197d329d4d2f9937332968c
손진영 <sm099@naver.com>
2024-07-30 23:31:29 +0900

    fix(useGetTravelogue): queryFn 변경

--- frontend/src/queries/useGetTravelogue.ts
+++ frontend/src/queries/useGetTravelogue.ts
@@ -2,11 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 
 import { Travelogue } from "@type/domain/travelogue";
 
-import { getTravelogue } from "@apis/travelogue";
+import { client } from "@apis/client";
 
 export const useGetTravelogue = (id: string) => {
   return useQuery<Travelogue>({
     queryKey: [`travelogues/${id}`],
-    queryFn: () => getTravelogue(id),
+    queryFn: async () => {
+      const { data } = await client.get(`/travelogues/${id}`);
+      return data;
+    },
   });
 };
