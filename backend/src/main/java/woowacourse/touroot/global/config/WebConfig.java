eb88bd68f260c815feaf442bc49386083d58c68d
eunjungL <62099953+eunjungL@users.noreply.github.com>
2024-07-30 15:41:11 +0900

    [Fix] - Authorization 오류 해결 (#156)
    * fix: JwtAuthFilter white list 로그인 method post로 변경
    
    * fix: CORS 설정에 Authorization header 추가

--- backend/src/main/java/woowacourse/touroot/global/config/WebConfig.java
+++ backend/src/main/java/woowacourse/touroot/global/config/WebConfig.java
@@ -1,6 +1,7 @@
 package woowacourse.touroot.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://dev.touroot.kr");
+                .allowedOrigins("http://localhost:3000", "https://dev.touroot.kr")
+                .allowedHeaders(HttpHeaders.AUTHORIZATION);
     }
 }
