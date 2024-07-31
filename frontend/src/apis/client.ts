7c354ccee9bc05194c3058ba9a12c6af7a6a903a
손진영 <sm099@naver.com>
2024-07-31 18:41:28 +0900

    feat: 전역 axios interceptor 설정 추가
    1. request 전 accessToken 확인 처리 및 authorization header 내 access token 추가
    
    2. APIError 발생 시 sentry 스택트레이서 추가
    
    3. AxiosError가 아닌 APIError를 throw

--- frontend/src/apis/client.ts
+++ frontend/src/apis/client.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
 
+import { handleAPIError, handlePreviousRequest } from "@apis/interceptor";
+
 export const client = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
+
+export const authClient = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+authClient.interceptors.request.use(handlePreviousRequest, handleAPIError);
+authClient.interceptors.response.use((response) => response, handleAPIError);
+
+client.interceptors.response.use((response) => response, handleAPIError);
