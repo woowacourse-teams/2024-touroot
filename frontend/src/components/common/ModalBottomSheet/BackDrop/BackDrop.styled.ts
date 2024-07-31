2c920a29c5afc54658be2fa3071f102a96a6367d
simhorim <hori5020@gmail.com>
2024-07-31 15:08:32 +0900

    refactor: z-index 추가

--- frontend/src/components/common/ModalBottomSheet/BackDrop/BackDrop.styled.ts
+++ frontend/src/components/common/ModalBottomSheet/BackDrop/BackDrop.styled.ts
@@ -2,10 +2,8 @@ import styled from "@emotion/styled";
 
 export const BackDrop = styled.div`
   position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+  inset: 0;
+  z-index: 600;
 
   background-color: ${({ theme }) => theme.colors.dimmed};
 `;
