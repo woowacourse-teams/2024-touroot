9e18e96b9ebfedb1d1706ffd247e6865843ef76a
slimsha2dy <chy2581@naver.com>
2024-07-31 17:37:52 +0900

    feat: 게시글 업로드 시 복사 메서드 호출하도록 기능 추가
    - 새로운 url을 db에 저장하도록 수정

--- backend/src/main/java/kr/touroot/travelogue/service/TraveloguePhotoService.java
+++ backend/src/main/java/kr/touroot/travelogue/service/TraveloguePhotoService.java
@@ -3,25 +3,29 @@ package kr.touroot.travelogue.service;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import kr.touroot.image.infrastructure.AwsS3Provider;
 import kr.touroot.travelogue.domain.TraveloguePhoto;
 import kr.touroot.travelogue.domain.TraveloguePlace;
 import kr.touroot.travelogue.dto.request.TraveloguePhotoRequest;
 import kr.touroot.travelogue.repository.TraveloguePhotoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @RequiredArgsConstructor
 @Service
 public class TraveloguePhotoService {
 
     private final TraveloguePhotoRepository traveloguePhotoRepository;
+    private final AwsS3Provider s3Provider;
 
     public List<TraveloguePhoto> createPhotos(List<TraveloguePhotoRequest> requests, TraveloguePlace place) {
         List<TraveloguePhoto> photos = new ArrayList<>();
 
         for (int i = 0; i < requests.size(); i++) {
             TraveloguePhotoRequest request = requests.get(i);
-            TraveloguePhoto photo = request.toTraveloguePhoto(i, place);
+            String url = s3Provider.copyImageToPermanentStorage(request.url());
+            TraveloguePhoto photo = new TraveloguePhoto(i, url, place);
             photos.add(traveloguePhotoRepository.save(photo));
         }
 
