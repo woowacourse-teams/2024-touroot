0d12877159253ce8f64896464dc5d42f0a4686f5
손진영 <sm099@naver.com>
2024-07-31 18:53:55 +0900

    feat(usePostTravelPlan): 여행 계획 추가에 대한 인가 처리 추가

--- frontend/src/queries/usePostTravelPlan.ts
+++ frontend/src/queries/usePostTravelPlan.ts
@@ -4,20 +4,24 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { TravelRegister, TravelRegisterPlace } from "@type/domain/travelogue";
 
-import { client } from "@apis/client";
+import ApiError from "@apis/ApiError";
+import { authClient } from "@apis/client";
 
 export const usePostTravelPlan = () => {
   const queryClient = useQueryClient();
   return useMutation<
     AxiosResponse<TravelRegisterPlace & { id: number }, unknown>,
-    Error,
+    ApiError,
     Omit<TravelRegister, "thumbnail"> & { startDate: string },
     unknown
   >({
     mutationFn: (travelPlan: Omit<TravelRegister, "thumbnail"> & { startDate: string }) =>
-      client.post("/travel-plans", travelPlan),
+      authClient.post("/travel-plans", travelPlan),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["travel-plans"] });
+    },
+    onError: (error) => {
+      alert(error.message);
     },
   });
 };
