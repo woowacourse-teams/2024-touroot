ca6d9742fbab22bb3dd1d7bdd382a207a35d8723
slimsha2dy <chy2581@naver.com>
2024-08-01 14:05:20 +0900

    style: 썸네일 철자 수정

--- backend/src/main/java/kr/touroot/travelogue/dto/request/TravelogueRequest.java
+++ backend/src/main/java/kr/touroot/travelogue/dto/request/TravelogueRequest.java
@@ -11,8 +11,8 @@ public record TravelogueRequest(
         @Schema(description = "여행기 제목", example = "서울 강남 여행기")
         @NotNull(message = "여행기 제목은 비어있을 수 없습니다.")
         String title,
-        @Schema(description = "여행기 섬네일", example = "https://thumbnail.png")
-        @NotNull(message = "여행기 섬네일은 비어있을 수 없습니다.")
+        @Schema(description = "여행기 썸네일", example = "https://thumbnail.png")
+        @NotNull(message = "여행기 썸네일은 비어있을 수 없습니다.")
         String thumbnail,
         @Schema(description = "여행기 일자 목록")
         @NotNull(message = "여행기 일자 목록은 비어있을 수 없습니다.")
--- backend/src/main/java/kr/touroot/travelogue/dto/response/TravelogueResponse.java
+++ backend/src/main/java/kr/touroot/travelogue/dto/response/TravelogueResponse.java
@@ -15,8 +15,8 @@ public record TravelogueResponse(
         @Schema(description = "여행기 제목", example = "서울 강남 여행기")
         @NotNull(message = "여행기 제목은 비어있을 수 없습니다.")
         String title,
-        @Schema(description = "여행기 섬네일 링크", example = "https://섬네일.png")
-        @NotNull(message = "여행기 섬네일 링크는 비어있을 수 없습니다.")
+        @Schema(description = "여행기 썸네일 링크", example = "https://썸네일.png")
+        @NotNull(message = "여행기 썸네일 링크는 비어있을 수 없습니다.")
         String thumbnail,
         @Schema(description = "여행기 일자 목록")
         @NotNull(message = "여행기 일자 목록은 비어있을 수 없습니다.")
