621912c9f194aea5f587e41440e0580c4d0a240d
손진영 <sm099@naver.com>
2024-08-03 15:12:39 +0900

    refactor: 여행기, 여행 계획 endpoint 내 id type을 string으로 변경

--- frontend/src/constants/endpoint.ts
+++ frontend/src/constants/endpoint.ts
@@ -1,7 +1,7 @@
 export const API_ENDPOINT_MAP = {
   loginOauth: (code: string) => `/login/oauth/kakao?code=${code}`,
-  travelogueDetail: (id: number) => `/travelogues/${id}`,
-  travelPlanDetail: (id: number) => `travel-plans/${id}`,
+  travelogueDetail: (id: string) => `/travelogues/${id}`,
+  travelPlanDetail: (id: string) => `travel-plans/${id}`,
   travelogues: "/travelogues",
   travelPlans: "/travel-plans",
   image: "/image",
--- frontend/src/queries/useGetTravelPlan.ts
+++ frontend/src/queries/useGetTravelPlan.ts
@@ -12,6 +12,6 @@ import { QUERY_KEYS_MAP } from "@constants/queryKey";
 export const useGetTravelPlan = (id: string) => {
   return useQuery<AxiosResponse<TravelPlanResponse>>({
     queryKey: QUERY_KEYS_MAP.travelPlan.detail(id),
-    queryFn: async () => authClient.get(API_ENDPOINT_MAP.travelPlanDetail(Number(id))),
+    queryFn: async () => authClient.get(API_ENDPOINT_MAP.travelPlanDetail(id)),
   });
 };
