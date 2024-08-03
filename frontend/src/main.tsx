6bcb36a2fe767080464957fc1b0be01dda056931
손진영 <sm099@naver.com>
2024-08-03 14:53:19 +0900

    feat: 개발 환경에서 browser 모킹 가능하도록 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { Global, ThemeProvider } from "@emotion/react";
 
 import * as Sentry from "@sentry/react";
+import { startMocking } from "@utils/worker";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -12,14 +13,6 @@ import { globalStyle } from "@styles/globalStyle";
 import theme from "@styles/theme";
 
 import App from "./App";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
@@ -35,12 +28,22 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 });
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools initialIsOpen={true} />
-    <ThemeProvider theme={theme}>
-      <Global styles={globalStyle} />
-      <App />
-    </ThemeProvider>
-  </QueryClientProvider>,
-);
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+startMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <ThemeProvider theme={theme}>
+        <Global styles={globalStyle} />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>,
+  );
+});
