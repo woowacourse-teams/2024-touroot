0bbd0dc312383489d47a430a58566b4aba9141a9
손진영 <sm099@naver.com>
2024-08-04 18:06:02 +0900

    refactor(Input): forwardRef 추가
    input 내 ref를 추가하기 위해 컴포넌트 내 forwardRef wrapping

--- frontend/src/components/common/Input/Input.tsx
+++ frontend/src/components/common/Input/Input.tsx
@@ -1,22 +1,26 @@
-import { InputHTMLAttributes } from "react";
+import { forwardRef } from "react";
 
 import CharacterCount from "../CharacterCount/CharacterCount";
 import * as S from "./Input.styled";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends React.ComponentPropsWithRef<'input'> {
   count?: number;
   maxCount?: number;
   label?: string;
 }
 
-const Input = ({ label, count, maxCount, ...props }: InputProps) => {
-  return (
-    <S.InputContainer>
-      <S.Label>{label}</S.Label>
-      <S.Input {...props} />
-      <CharacterCount count={count} maxCount={maxCount} />
-    </S.InputContainer>
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, count, maxCount, ...props }, ref) => {
+    return (
+      <S.InputContainer>
+        <S.Label>{label}</S.Label>
+        <S.Input {...props} ref={ref} />
+        <CharacterCount count={count} maxCount={maxCount} />
+      </S.InputContainer>
+    );
+  },
+);
+
+Input.displayName = "Input";
 
 export default Input;
