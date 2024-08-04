3430b7f4ea7868e763cacdfcef0f946d710420dc
손진영 <sm099@naver.com>
2024-08-04 15:24:49 +0900

    refactor(ROUTE_PATHS_MAP): travelogue 및 travelPlan의 id variable 타입 변경
    uuid와 id 모두 받을 수 있도록 타입 변경

--- frontend/src/constants/route.ts
+++ frontend/src/constants/route.ts
@@ -1,8 +1,8 @@
 export const ROUTE_PATHS_MAP = {
   back: -1,
   root: "/",
-  travelogue: (id?: number) => (id ? `/travelogue/${id}` : "/travelogue/:id"),
-  travelPlan: (id?: number) => (id ? `/travel-plan/${id}` : "/travel-plan/:id"),
+  travelogue: (id?: number | string) => (id ? `/travelogue/${id}` : "/travelogue/:id"),
+  travelPlan: (id?: number | string) => (id ? `/travel-plan/${id}` : "/travel-plan/:id"),
   travelogueRegister: "/travelogue/register",
   travelPlanRegister: "/travel-plan/register",
   login: "/login",
