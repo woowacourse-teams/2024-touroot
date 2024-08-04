6b780c473f4f556f7d32cb48c228a8fb8d316a3f
river <130737187+0jenn0@users.noreply.github.com>
2024-08-04 15:30:24 +0900

    feat: 마이페이지 tab content에서 사용될 TabContent 컴포넌트 구현

---
+++ frontend/src/components/pages/my/TabContent/TabContent.styled.ts
@@ -0,0 +1,43 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+import theme from "@styles/theme";
+
+export const Container = styled.div`
+  display: flex;
+  gap: ${(props) => props.theme.spacing.s};
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: ${(props) => props.theme.spacing.m};
+`;
+
+export const ColorButtonStyle = css`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 6.8rem;
+  padding: ${theme.spacing.m};
+  border-radius: 10px;
+
+  background-color: #f3faff;
+  gap: ${theme.spacing.m};
+`;
+
+export const BoxButton = styled.button`
+  display: flex;
+  gap: ${(props) => props.theme.spacing.m};
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  padding: ${(props) => props.theme.spacing.m};
+  border: solid 1px ${(props) => props.theme.colors.border};
+  border-radius: 10px;
+`;
---
+++ frontend/src/components/pages/my/TabContent/TabContent.tsx
@@ -0,0 +1,52 @@
+import { css } from "@emotion/react";
+
+import { IconButton } from "@components/common";
+
+import { SEMANTIC_COLORS } from "@styles/tokens";
+
+import * as S from "./TabContent.styled";
+
+const ICON_BUTTON_TEXT = {
+  ADD_TRAVEL_PLAN: "새 여행 계획 추가하기",
+  ADD_TRAVELOGUE: "새 여행기 추가하기",
+} as const;
+
+interface TabContentProps<T extends { id: string }> {
+  iconButtonLabel: (typeof ICON_BUTTON_TEXT)[keyof typeof ICON_BUTTON_TEXT];
+  onClickIconButton: () => void;
+  data: T[];
+  renderItem: (item: Omit<T, "id">) => React.ReactNode;
+}
+
+const TabContent = <T extends { id: string }>({
+  data,
+  iconButtonLabel,
+  onClickIconButton,
+  renderItem,
+}: React.PropsWithChildren<TabContentProps<T>>) => {
+  return (
+    <S.List>
+      <IconButton
+        size="16"
+        position="left"
+        iconType="plus"
+        color={SEMANTIC_COLORS.primary}
+        css={[
+          S.ColorButtonStyle,
+          css`
+            font-weight: 600;
+          `,
+        ]}
+        onClick={onClickIconButton}
+      >
+        {iconButtonLabel}
+      </IconButton>
+
+      {data.map(({ id, ...rest }) => (
+        <S.BoxButton key={id}>{renderItem(rest)}</S.BoxButton>
+      ))}
+    </S.List>
+  );
+};
+
+export default TabContent;
