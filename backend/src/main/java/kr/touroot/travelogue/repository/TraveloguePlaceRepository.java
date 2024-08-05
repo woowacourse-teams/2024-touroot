5e284a6391a6a49784dc87eb4ff7e8b1a99188e1
nhlee98 <e5slnh9876@gmail.com>
2024-08-05 17:14:37 +0900

    feat: 각 리포지토리에 여행기 id로 삭제하는 메소드 추가

--- backend/src/main/java/kr/touroot/travelogue/repository/TraveloguePlaceRepository.java
+++ backend/src/main/java/kr/touroot/travelogue/repository/TraveloguePlaceRepository.java
@@ -1,11 +1,13 @@
 package kr.touroot.travelogue.repository;
 
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
 import kr.touroot.travelogue.domain.TravelogueDay;
 import kr.touroot.travelogue.domain.TraveloguePlace;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TraveloguePlaceRepository extends JpaRepository<TraveloguePlace, Long> {
 
     List<TraveloguePlace> findByTravelogueDay(TravelogueDay travelogueDay);
+
+    void deleteByTravelogueDayTravelogueId(Long travelogueId);
 }
