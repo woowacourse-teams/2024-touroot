9118715f27b9b9d93cd36e0390a45d58dc1c91fb
손진영 <sm099@naver.com>
2024-08-06 18:14:39 +0900

    test(travelPlanRegisterPage): 여행 계획 등록 페이지 테스트 구현
    1. 여행 계획 등록 msw 핸들러 추가
    
    2. usePostTravelPlan를 wrapping한 createTravelPlanRegisterHook 추가
    
    3. 여행 계획 등록 페이지 테스트 코드 구현

--- frontend/src/mocks/handlers/index.ts
+++ frontend/src/mocks/handlers/index.ts
@@ -1,3 +1,4 @@
+import { travelPlanRegisterHandler } from "@mocks/handlers/travelPlanRegisterHandler";
 import { travelogueInfiniteHandler } from "@mocks/handlers/travelogueInfiniteHandler";
 
-export const handlers = [travelogueInfiniteHandler];
+export const handlers = [travelogueInfiniteHandler, travelPlanRegisterHandler];
