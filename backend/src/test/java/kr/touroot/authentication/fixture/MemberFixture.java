1371e7d5cb072745296496e0103cfed4febcc4ee
hangillee <skfcb10@naver.com>
2024-08-06 16:49:17 +0900

    refactor: 사용자 test fixture 추가

--- backend/src/test/java/kr/touroot/authentication/fixture/MemberFixture.java
+++ backend/src/test/java/kr/touroot/authentication/fixture/MemberFixture.java
@@ -5,5 +5,12 @@ import kr.touroot.member.domain.Member;
 
 public class MemberFixture {
 
-    public static final Member MEMBER_1 = new Member(1L, "리비", "http://imageurl.com", LoginType.KAKAO);
+    public static final Member MEMBER_KAKAO = new Member(1L, "리비", "http://imageurl.com", LoginType.KAKAO);
+    public static final Member MEMBER_DEFAULT = new Member(
+            "user@email.com",
+            "5304d46adc6ccffd0abf352f3e17ba8b807dc3d5e5d12609d0825d7287",
+            "뚜리",
+            "http://imageurl.com",
+            LoginType.DEFAULT
+    );
 }
