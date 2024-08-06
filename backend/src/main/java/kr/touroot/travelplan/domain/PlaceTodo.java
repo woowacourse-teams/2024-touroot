d31ceed125352c2376f6d63f53727893605570a9
libienz <leekh10195@gmail.com>
2024-08-06 17:19:09 +0900

    feat: TODO 도메인 작성 및 검증 구현

---
+++ backend/src/main/java/kr/touroot/travelplan/domain/PlaceTodo.java
@@ -0,0 +1,87 @@
+package kr.touroot.travelplan.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.touroot.global.entity.BaseEntity;
+import kr.touroot.global.exception.BadRequestException;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class PlaceTodo extends BaseEntity {
+
+    private static final int CONTENT_MIN_LENGTH = 1;
+    private static final int CONTENT_MAX_LENGTH = 20;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private TravelPlanPlace travelPlanPlace;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(name = "TODO_ORDER", nullable = false)
+    private Integer order;
+
+    @Column(nullable = false)
+    private Boolean isChecked;
+
+    private PlaceTodo(Long id, TravelPlanPlace travelPlanPlace, String content, Integer order, Boolean isChecked) {
+        validate(travelPlanPlace, content, order, isChecked);
+        this.id = id;
+        this.travelPlanPlace = travelPlanPlace;
+        this.content = content;
+        this.order = order;
+        this.isChecked = isChecked;
+    }
+
+    public PlaceTodo(TravelPlanPlace travelPlanPlace, String content, Integer order, Boolean isChecked) {
+        this(null, travelPlanPlace, content, order, isChecked);
+    }
+
+    private void validate(TravelPlanPlace travelPlanPlace, String content, Integer order, Boolean isChecked) {
+        validateNotNull(travelPlanPlace, content, order, isChecked);
+        validateNotBlank(content);
+        validateContentLength(content);
+        validateOrderNonNegative(order);
+    }
+
+    private void validateNotNull(TravelPlanPlace travelPlanPlace, String content, Integer order, Boolean isChecked) {
+        if (travelPlanPlace == null || content == null || order == null || isChecked == null) {
+            throw new BadRequestException("여행 계획 장소에 대한 TODO에서 장소와 내용, 순서 그리고 달성 여부는 비어 있을 수 없습니다");
+        }
+    }
+
+    private void validateNotBlank(String content) {
+        if (content.isBlank()) {
+            throw new BadRequestException("TODO 내용은 빈 문자열로만 이루어질 수 없습니다");
+        }
+    }
+
+    private void validateContentLength(String content) {
+        if (CONTENT_MIN_LENGTH > content.length() || content.length() > CONTENT_MAX_LENGTH) {
+            throw new BadRequestException(
+                    "TODO 내용의 길이는 " + CONTENT_MIN_LENGTH + "자 이상, " + CONTENT_MAX_LENGTH + "자 이하여야 합니다"
+            );
+        }
+    }
+
+    private void validateOrderNonNegative(Integer order) {
+        if (order < 0) {
+            throw new BadRequestException("TODO 순서는 음수일 수 없습니다");
+        }
+    }
+}
