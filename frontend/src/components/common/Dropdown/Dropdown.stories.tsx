df15d37e8b46ef935133f7d2889633d7d9ae6d31
simhorim <hori5020@gmail.com>
2024-08-06 15:17:16 +0900

    test(Dropdown): 스토리북 테스트 구현

---
+++ frontend/src/components/common/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Text from "../Text/Text";
+import Dropdown from "./Dropdown";
+
+const meta = {
+  title: "common/Dropdown",
+  component: Dropdown,
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component: "투룻에서 사용하는 드랍다운 컴포넌트로, position과 size에 따라 구분",
+      },
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    position: {
+      description: "드랍다운의 위치",
+    },
+    size: {
+      description: "드랍다운의 크기",
+    },
+  },
+} satisfies Meta<typeof Dropdown>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    isOpen: true,
+    position: "right",
+    size: "small",
+    children: (
+      <>
+        <Text textType="detail" onClick={() => {}} style={{ cursor: "pointer" }}>
+          수정
+        </Text>
+        <Text textType="detail" onClick={() => {}} style={{ cursor: "pointer" }}>
+          삭제
+        </Text>
+      </>
+    ),
+  },
+};
