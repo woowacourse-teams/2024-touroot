ea86c3acd1b7d61021c8e1de2e2bda6a49147f53
river <130737187+0jenn0@users.noreply.github.com>
2024-08-06 18:22:18 +0900

    feat: MyTravelogues 컴포넌트 구현

---
+++ frontend/src/components/pages/my/MyTravelogues/MyTravelogues.tsx
@@ -0,0 +1,81 @@
+import { useNavigate } from "react-router-dom";
+
+import { css } from "@emotion/react";
+
+import useInfiniteMyTravelogues from "@queries/useInfiniteMyTravelogues";
+
+import { AvatarCircle, Text } from "@components/common";
+
+import useIntersectionObserver from "@hooks/useIntersectionObserver";
+
+import { ROUTE_PATHS_MAP } from "@constants/route";
+
+import theme from "@styles/theme";
+
+import TabContent from "../TabContent/TabContent";
+import * as S from "./MyTravelogues.styled";
+
+const MyTravelogues = () => {
+  const { myTravelogues, fetchNextPage, status } = useInfiniteMyTravelogues();
+  const { lastElementRef } = useIntersectionObserver(fetchNextPage);
+
+  const navigate = useNavigate();
+
+  const handleClickAddButton = () => {
+    navigate(ROUTE_PATHS_MAP.travelogueRegister);
+  };
+
+  const handleClickTravelogue = (id: string) => {
+    navigate(ROUTE_PATHS_MAP.travelogue(Number(id)));
+  };
+
+  if (status === "pending") return <div>로딩 중...</div>;
+
+  return (
+    <>
+      <TabContent
+        iconButtonLabel="새 여행기 추가하기"
+        onClickIconButton={handleClickAddButton}
+        data={myTravelogues}
+        renderItem={({ id, thumbnailUrl, title, createdAt }) => (
+          <S.Layout onClick={() => handleClickTravelogue(id)}>
+            <AvatarCircle $size="medium" profileImageUrl={thumbnailUrl} />
+            <S.Container>
+              <Text
+                textType="body"
+                css={css`
+                  font-weight: 500;
+                `}
+              >
+                {title}
+              </Text>
+              <Text
+                textType="detail"
+                css={css`
+                  color: ${theme.colors.text.secondary};
+                `}
+              >
+                게시일 {createdAt}
+              </Text>
+            </S.Container>
+          </S.Layout>
+        )}
+      />
+      <div
+        ref={lastElementRef}
+        css={css`
+          height: 1px;
+        `}
+      />
+    </>
+  );
+};
+
+export default MyTravelogues;
+
+export interface MyTravelogue {
+  id: string;
+  thumbnailUrl: string;
+  title: string;
+  createdAt: string;
+}
