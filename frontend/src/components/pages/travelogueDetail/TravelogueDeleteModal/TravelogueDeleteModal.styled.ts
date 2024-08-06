720eb996dc90fccb047a8733e303012e37e521b9
simhorim <hori5020@gmail.com>
2024-08-06 15:18:51 +0900

    feat(TravelogueDeleteModal): 컴포넌트 구현

---
+++ frontend/src/components/pages/travelogueDetail/TravelogueDeleteModal/TravelogueDeleteModal.styled.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ frontend/src/components/pages/travelogueDetail/TravelogueDeleteModal/TravelogueDeleteModal.tsx
@@ -0,0 +1,51 @@
+import { css } from "@emotion/react";
+
+import { Button, Text } from "@components/common";
+import Modal from "@components/common/Modal/Modal";
+
+import theme from "@styles/theme";
+
+import { Tturi } from "@assets/svg";
+
+import * as S from "./TravelogueDeleteModal.styled";
+
+interface TravelogueDeleteModalProps {
+  isOpen: boolean;
+  onCloseModal: () => void;
+  onClickDeleteButton: () => void;
+}
+
+const TravelogueDeleteModal = ({
+  isOpen,
+  onCloseModal,
+  onClickDeleteButton,
+}: TravelogueDeleteModalProps) => {
+  return isOpen ? (
+    <Modal isOpen={isOpen} onCloseModal={onCloseModal}>
+      <Modal.Header />
+      <Modal.Body direction="column" style={{ gap: "1.6rem", padding: "2.6rem 0" }}>
+        <Tturi />
+        <S.TextContainer>
+          <Text textType="body" style={{ fontWeight: 700 }}>
+            여행기를 삭제할까요?
+          </Text>
+          <Text
+            textType="detail"
+            css={css`
+              color: ${theme.colors.text.secondary};
+            `}
+          >
+            삭제한 후에는 여행기를 다시 복구할 수 없어요.
+          </Text>
+        </S.TextContainer>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variants="primary" onClick={onClickDeleteButton}>
+          삭제
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  ) : null;
+};
+
+export default TravelogueDeleteModal;
