58614ecfe5fd26c42902561b945a92ba422e8ffb
eunjungL <62099953+eunjungL@users.noreply.github.com>
2024-08-06 14:09:15 +0900

    [Feature] - 마이페이지 API Swagger 작업  (#218)
    * feat: 여행 계획 조회 및 여행기 조회 swagger 문서 추가
    
    * docs: 나의 프로필 정보 조회 swagger 작성 (feat. 리버)

--- backend/src/main/java/kr/touroot/member/controller/MyPageController.java
+++ backend/src/main/java/kr/touroot/member/controller/MyPageController.java
@@ -1,9 +1,15 @@
 package kr.touroot.member.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import kr.touroot.global.auth.dto.MemberAuth;
+import kr.touroot.global.exception.dto.ExceptionResponse;
 import kr.touroot.member.dto.MyTravelPlanResponse;
 import kr.touroot.member.dto.MyTraveloguesResponse;
 import kr.touroot.member.dto.ProfileResponse;
@@ -22,17 +28,41 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "마이 페이지")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/v1/member/me")
+@RequestMapping("/api/v1/member/me")
 public class MyPageController {
 
     private final MyPageFacadeService myPageFacadeService;
 
+    @Operation(summary = "나의 프로필 정보 조회")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "내 프로필 정보 조회에 성공했을 때"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "로그인하지 않은 사용자가 프로필 정보 조회에 시도할 때",
+                    content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
+            )
+    })
     @GetMapping("/profile")
     public ResponseEntity<ProfileResponse> readProfile(@NotNull MemberAuth memberAuth) {
         ProfileResponse data = myPageFacadeService.readProfile(memberAuth);
         return ResponseEntity.ok(data);
     }
 
+    @Operation(summary = "내 여행기 조회")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "내 여행기 조회에 성공했을 때"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "로그인하지 않은 사용자가 조회를 시도할 때",
+                    content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
+            )
+    })
     @PageableAsQueryParam
     @GetMapping("/travelogues")
     public ResponseEntity<Page<MyTraveloguesResponse>> readTravelogues(
@@ -45,6 +75,18 @@ public class MyPageController {
         return ResponseEntity.ok(data);
     }
 
+    @Operation(summary = "내 여행 계획 조회")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "내 여행 계획 조회에 성공했을 때"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "로그인하지 않은 사용자가 조회를 시도할 때",
+                    content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
+            )
+    })
     @PageableAsQueryParam
     @GetMapping("/travel-plans")
     public ResponseEntity<Page<MyTravelPlanResponse>> readTravelPlans(
