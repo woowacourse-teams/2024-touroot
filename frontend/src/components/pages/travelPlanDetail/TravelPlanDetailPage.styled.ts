c06f8f07952746d797762ed125baafc2359f158d
simhorim <hori5020@gmail.com>
2024-08-06 15:22:48 +0900

    feat(TravelPlanDetailPage): Dropdown 기능 추가

--- frontend/src/components/pages/travelPlanDetail/TravelPlanDetailPage.styled.ts
+++ frontend/src/components/pages/travelPlanDetail/TravelPlanDetailPage.styled.ts
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 export const TitleContainer = styled.div`
   display: flex;
   flex-direction: column;
+
   gap: 0.8rem;
 
   width: 100%;
@@ -10,9 +11,11 @@ export const TitleContainer = styled.div`
   padding: 0 1.6rem;
 `;
 
-export const MeatballWrapper = styled.div`
+export const IconButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
+  position: relative;
 
   width: 100%;
+  gap: 0.8rem;
 `;
--- frontend/src/components/pages/travelPlanDetail/TravelPlanDetailPage.tsx
+++ frontend/src/components/pages/travelPlanDetail/TravelPlanDetailPage.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import { css } from "@emotion/react";
 
 import { useTravelTransformDetailContext } from "@contexts/TravelTransformDetailProvider";
+
+import useDeleteTravelPlan from "@queries/useDeleteTravelPlan";
 import { useGetTravelPlan } from "@queries/useGetTravelPlan";
 
-import { Tab, Text, TransformBottomSheet } from "@components/common";
+import { IconButton, Tab, Text, TransformBottomSheet } from "@components/common";
+import Dropdown from "@components/common/Dropdown/Dropdown";
 import TravelPlansTabContent from "@components/pages/travelPlanDetail/TravelPlansTabContent/TravelPlansTabContent";
 
+import theme from "@styles/theme";
 import { PRIMITIVE_COLORS } from "@styles/tokens";
 
+import TravelPlanDeleteModal from "./TravelPlanDeleteModal/TravelPlanDeleteModal";
 import * as S from "./TravelPlanDetailPage.styled";
 
 const TravelPlanDetailPage = () => {
@@ -25,29 +31,57 @@ const TravelPlanDetailPage = () => {
       : "당일치기";
 
   const { onTransformTravelDetail } = useTravelTransformDetailContext();
+  const { mutate: deleteTravelPlan } = useDeleteTravelPlan();
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const handleToggleMoreDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
+  const handleToggleModal = () => {
+    setIsDeleteModalOpen((prev) => !prev);
+  };
+
+  const handleClickDeleteButton = () => {
+    deleteTravelPlan(Number(id));
+  };
+
+  //TODO: 수정 이벤트 추가해야함
+  const handleClickReviseButton = () => {};
 
   return (
     <>
       <S.TitleContainer>
-        <Text textType="title">{data?.data?.title}</Text>
-        <S.MeatballWrapper>
-          <Text
-            textType="title"
-            css={css`
-              color: ${PRIMITIVE_COLORS.gray[500]};
-            `}
-          >
-            ...
-          </Text>
-        </S.MeatballWrapper>
+        <Text
+          textType="subTitle"
+          css={css`
+            line-height: 2.4rem;
+          `}
+        >
+          {data?.data?.title}
+        </Text>
+        <S.IconButtonContainer>
+          <IconButton iconType="share" size="16" color={PRIMITIVE_COLORS.black} />
+          <IconButton
+            iconType="more"
+            size="16"
+            color={theme.colors.text.secondary}
+            onClick={handleToggleMoreDropdown}
+          />
+          <Dropdown isOpen={isDropdownOpen} size="small" position="right">
+            <Text textType="detail" onClick={handleClickReviseButton} style={{ cursor: "pointer" }}>
+              수정
+            </Text>
+            <Text textType="detail" onClick={handleToggleModal} style={{ cursor: "pointer" }}>
+              삭제
+            </Text>
+          </Dropdown>
+        </S.IconButtonContainer>
       </S.TitleContainer>
 
-      <Text
-        textType="subTitle"
-        css={css`
-          margin: 3.2rem 1.6rem 1.6rem;
-        `}
-      >
+      <Text textType="title" style={{ margin: "3.2rem 1.6rem 1.6rem" }}>
         {daysAndNights} 여행 계획 한 눈에 보기
       </Text>
 
@@ -63,6 +97,11 @@ const TravelPlanDetailPage = () => {
       >
         여행은 즐겁게 다녀오셨나요?
       </TransformBottomSheet>
+      <TravelPlanDeleteModal
+        isOpen={isDeleteModalOpen}
+        onCloseModal={handleToggleModal}
+        onClickDeleteButton={handleClickDeleteButton}
+      />
     </>
   );
 };
