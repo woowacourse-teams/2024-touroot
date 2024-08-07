9a486b32c85145ef0eda7037c47a1630f1eea85d
손진영 <sm099@naver.com>
2024-08-07 09:37:08 +0900

    refactor(endpoint): 여행 계획 상세 및 공유된 여행 계획 상세 prefix(/) 추가

--- frontend/src/constants/endpoint.ts
+++ frontend/src/constants/endpoint.ts
@@ -2,8 +2,8 @@ export const API_ENDPOINT_MAP = {
   loginOauth: (code: string, redirectUri: string) =>
     `/login/oauth/kakao?code=${code}&redirectUri=${redirectUri}`,
   travelogueDetail: (id: number | string) => `/travelogues/${id}`,
-  travelPlanDetail: (id: number | string) => `travel-plans/${id}`,
-  sharedTravelPlanDetail: (uuid: string) => `travel-plans/shared/${uuid}`,
+  travelPlanDetail: (id: number | string) => `/travel-plans/${id}`,
+  sharedTravelPlanDetail: (uuid: string) => `/travel-plans/shared/${uuid}`,
   travelogues: "/travelogues",
   travelPlans: "/travel-plans",
   image: "/image",
