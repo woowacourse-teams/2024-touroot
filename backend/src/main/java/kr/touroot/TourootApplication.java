6647d976a8f8805aa576581d91659a41c3f516b5
eunjungL <dldmswjd322@gmail.com>
2024-08-07 12:13:39 +0900

    feat: 태그 조회 API 캐싱 추가

--- backend/src/main/java/kr/touroot/TourootApplication.java
+++ backend/src/main/java/kr/touroot/TourootApplication.java
@@ -2,8 +2,10 @@ package kr.touroot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class TourootApplication {
--- backend/src/main/java/kr/touroot/tag/service/TagService.java
+++ backend/src/main/java/kr/touroot/tag/service/TagService.java
@@ -7,9 +7,12 @@ import kr.touroot.tag.dto.TagCreateRequest;
 import kr.touroot.tag.dto.TagResponse;
 import kr.touroot.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class TagService {
@@ -30,6 +33,7 @@ public class TagService {
         }
     }
 
+    @Cacheable(cacheNames = "tag")
     @Transactional(readOnly = true)
     public List<TagResponse> readTags() {
         return tagRepository.findAll().stream()
