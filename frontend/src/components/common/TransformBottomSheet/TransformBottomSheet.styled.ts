dc58f5eb7a914b2a2b14e33d5e866a37b87def64
simhorim <hori5020@gmail.com>
2024-08-08 19:56:03 +0900

    refactor(TransformBottomSheet): IconButton으로 수정, 불 필요한 css 제거 및 up-arrow layout에 붙은 문제 해결

--- frontend/src/components/common/TransformBottomSheet/TransformBottomSheet.styled.ts
+++ frontend/src/components/common/TransformBottomSheet/TransformBottomSheet.styled.ts
@@ -23,68 +23,27 @@ const slideDown = keyframes`
 
 export const BottomSheetLayout = styled.div<{ $isOpen: boolean }>`
   display: flex;
-  overflow: hidden;
-  position: fixed;
-  bottom: 0;
-  z-index: 1000;
-  width: 100%;
-  height: ${({ $isOpen }) => ($isOpen ? "12rem" : "5rem")};
-
-  background-color: white;
-
-  animation: ${({ $isOpen }) => ($isOpen ? slideUp : slideDown)} 0.3s ease-out;
-  max-width: 48rem;
-  display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
   overflow: hidden;
   position: fixed;
   bottom: 0;
-  z-index: 1000;
+  z-index: ${({ theme }) => theme.zIndex.bottomSheet};
+  gap: 1rem;
+
   width: 100%;
   height: ${({ $isOpen }) => ($isOpen ? "12rem" : "5rem")};
   max-width: 48rem;
+  padding: 1rem;
+  border-radius: 2rem 2rem 0 0;
 
   background-color: ${PRIMITIVE_COLORS.white};
 
   animation: ${({ $isOpen }) => ($isOpen ? slideUp : slideDown)} 0.3s ease-out;
-  border-top-left-radius: 20px;
-  border-top-right-radius: 20px;
-  box-shadow: 0 -2px 10px rgb(0 0 0 / 10%);
   box-shadow: 0 -2px 10px rgb(0 0 0 / 10%);
 `;
 
-export const BottomSheetContent = styled.div`
-  height: 100%;
-  height: 100%;
-  padding: 20px;
-
-  text-align: center;
-  overflow-y: auto;
-
-  ${({ theme }) => theme.typography.mobile.detailBold}
-`;
-
-export const BottomSheetBottomContainer = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
+export const BottomSheetButtonWrapper = styled.div`
   width: 100%;
-  padding: 10px;
-
-  background-color: white;
-`;
-
-export const BottomSheetButton = styled.button`
-  width: 100%;
-  padding: 10px;
-  border: none;
-
-  background-color: ${({ theme }) => theme.colors.primary};
-
-  color: white;
-  font-weight: bold;
-  font-size: 16px;
-  border-radius: 5px;
-  cursor: pointer;
 `;
