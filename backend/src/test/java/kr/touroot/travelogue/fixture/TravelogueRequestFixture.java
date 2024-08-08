3a33291d2bea46aba90917ab3d81a12cada70608
Lee Hangil <skfcb10@naver.com>
2024-08-08 17:33:16 +0900

    [Fix] - 이미지 최대 업로드 개수 검증 누락 해결 (#271)
    * fix: 여행기 작성 시, 최대 업로드 가능 개수 이상의 사진이 추가되면 예외 발생하도록 검증 추가
    
    * refactor: 검증 로직 추가에 따른 테스트 코드 수정

--- backend/src/test/java/kr/touroot/travelogue/fixture/TravelogueRequestFixture.java
+++ backend/src/test/java/kr/touroot/travelogue/fixture/TravelogueRequestFixture.java
@@ -1,12 +1,12 @@
 package kr.touroot.travelogue.fixture;
 
 import java.util.List;
-import org.springframework.stereotype.Component;
 import kr.touroot.travelogue.dto.request.TravelogueDayRequest;
 import kr.touroot.travelogue.dto.request.TraveloguePhotoRequest;
 import kr.touroot.travelogue.dto.request.TraveloguePlaceRequest;
 import kr.touroot.travelogue.dto.request.TraveloguePositionRequest;
 import kr.touroot.travelogue.dto.request.TravelogueRequest;
+import org.springframework.stereotype.Component;
 
 @Component
 public class TravelogueRequestFixture {
@@ -14,20 +14,20 @@ public class TravelogueRequestFixture {
     private TravelogueRequestFixture() {
     }
 
-    public static TravelogueRequest getTravelogueRequest() {
-        return new TravelogueRequest("제주에 하영 옵서", "https://dev.touroot.kr/temporary/jeju_thumbnail.png", getTravelogueDayRequests());
+    public static TravelogueRequest getTravelogueRequest(List<TravelogueDayRequest> days) {
+        return new TravelogueRequest("제주에 하영 옵서", "https://dev.touroot.kr/temporary/jeju_thumbnail.png", days);
     }
 
-    public static List<TravelogueDayRequest> getTravelogueDayRequests() {
-        return List.of(new TravelogueDayRequest(getTraveloguePlaceRequests()));
+    public static List<TravelogueDayRequest> getTravelogueDayRequests(List<TraveloguePlaceRequest> places) {
+        return List.of(new TravelogueDayRequest(places));
     }
 
-    public static List<TraveloguePlaceRequest> getTraveloguePlaceRequests() {
+    public static List<TraveloguePlaceRequest> getTraveloguePlaceRequests(List<TraveloguePhotoRequest> photos) {
         return List.of(new TraveloguePlaceRequest(
                 "함덕해수욕장",
                 getTraveloguePositionRequest(),
                 "에메랄드 빛 해변",
-                getTraveloguePhotoRequests()
+                photos
         ));
     }
 
@@ -37,5 +37,21 @@ public class TravelogueRequestFixture {
 
     public static List<TraveloguePhotoRequest> getTraveloguePhotoRequests() {
         return List.of(new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image1.png"));
+    }
+
+    public static List<TraveloguePhotoRequest> getTraveloguePhotoRequestsOverLimit() {
+        return List.of(
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image1.png"),
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image2.png"),
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image3.png"),
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image4.png"),
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image5.png"),
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image6.png"),
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image7.png"),
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image8.png"),
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image9.png"),
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image10.png"),
+                new TraveloguePhotoRequest("https://dev.touroot.kr/temporary/image11.png")
+        );
     }
 }
