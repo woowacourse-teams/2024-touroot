1bd483ae798535c033cd2c0453ff3b72f9459593
손진영 <sm099@naver.com>
2024-08-08 23:44:58 +0900

    refactor(TravelPlansTabContent): 여행 계획 내 description 추가

--- frontend/src/components/pages/travelPlanDetail/TravelPlansTabContent/TravelPlansTabContent.styled.ts
+++ frontend/src/components/pages/travelPlanDetail/TravelPlansTabContent/TravelPlansTabContent.styled.ts
@@ -1,8 +1,24 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+
+import theme from "@styles/theme";
 
 export const BoxContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 3.2rem 1.6rem 6.4rem;
   gap: 0.8rem;
+`;
+
+export const boxStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  height: auto;
+  padding: 1.2rem 1.6rem;
+  gap: 0.4rem;
+`;
+
+export const textStyle = css`
+  color: ${theme.colors.text.secondary};
 `;
