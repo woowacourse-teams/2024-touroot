1d96bd44a6e2f31df483278874d32129945951fd
jinyoung <87177577+jinyoung234@users.noreply.github.com>
2024-08-13 17:20:20 +0900

    [Fix] - 만료된 access token 핸들링 (#292)
    * fix(interceptor): 만료 토큰 에러 시 early return 하여 error를 throw 하지 않도록 변경
    
    * fix: onSuccess callback 내에서 구조 분해 할당을 하지 않도록 변경
    
    * refactor: 수정 ui 제거

--- frontend/src/components/pages/travelPlanDetail/TravelPlanDetailPage.tsx
+++ frontend/src/components/pages/travelPlanDetail/TravelPlanDetailPage.tsx
@@ -74,7 +74,7 @@ const TravelPlanDetailPage = () => {
   };
 
   //TODO: 수정 이벤트 추가해야함
-  const handleClickReviseButton = () => {};
+  // const handleClickReviseButton = () => {};
 
   const [isShareModalOpen, setIsShareModalOpen] = useState(false);
 
@@ -128,13 +128,13 @@ const TravelPlanDetailPage = () => {
               />
               {isDropdownOpen && (
                 <Dropdown size="small" position="right">
-                  <Text
+                  {/* <Text
                     textType="detail"
                     onClick={handleClickReviseButton}
                     css={S.cursorPointerStyle}
                   >
                     수정
-                  </Text>
+                  </Text> */}
                   <Text
                     textType="detail"
                     onClick={handleToggleDeleteModal}
