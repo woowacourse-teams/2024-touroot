691ab751170e048f0e3e842d7787fe0e924eb556
simhorim <hori5020@gmail.com>
2024-08-13 18:11:26 +0900

    refactor(useLeadingDebounce): 렌더링과 무관하기에 useState 에서 useRef로 수정

--- frontend/src/hooks/useLeadingDebounce.ts
+++ frontend/src/hooks/useLeadingDebounce.ts
@@ -1,17 +1,32 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 const useLeadingDebounce = (callback: () => void, delay: number) => {
-  const [isDebouncing, setIsDebouncing] = useState(false);
+  const isDebouncing = useRef(false);
+  const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
 
   const debouncedCallback = () => {
-    if (!isDebouncing) {
+    const isDebounceReady = !isDebouncing.current;
+    const isExistingTimerActive = isDebounceReady && timeoutRef.current;
+
+    if (isExistingTimerActive) clearTimeout(timeoutRef.current);
+
+    if (isDebounceReady) {
       callback();
-      setIsDebouncing(true);
-      setTimeout(() => {
-        setIsDebouncing(false);
+      isDebouncing.current = true;
+
+      timeoutRef.current = setTimeout(() => {
+        isDebouncing.current = false;
       }, delay);
     }
   };
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
 
   return debouncedCallback;
 };
