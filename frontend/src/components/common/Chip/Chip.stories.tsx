7face11a16553e3d07f65b9ac382fab7f359e0d3
simhorim <hori5020@gmail.com>
2024-08-14 21:08:27 +0900

    test(Chip): 스토리북 구현

---
+++ frontend/src/components/common/Chip/Chip.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Chip from "./Chip";
+
+const meta = {
+  title: "common/Chip",
+  component: Chip,
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component: "투룻에서 사용하는 칩 컴포넌트로, isSelected로 색상 구분",
+      },
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Chip>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Selected: Story = {
+  args: {
+    isSelected: true,
+    label: "🐾 반려동물",
+  },
+};
+
+export const UnSelected: Story = {
+  args: {
+    isSelected: false,
+    label: "🐾 반려동물",
+  },
+};
