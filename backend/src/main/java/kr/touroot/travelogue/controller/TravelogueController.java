2d103a6c1350e44f88a785d8448744fc231e3016
hangillee <skfcb10@naver.com>
2024-08-14 14:06:03 +0900

    refactor: 검색 키워드 request parameter DTO로 분리

--- backend/src/main/java/kr/touroot/travelogue/controller/TravelogueController.java
+++ backend/src/main/java/kr/touroot/travelogue/controller/TravelogueController.java
@@ -8,12 +8,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import java.net.URI;
 import kr.touroot.global.auth.dto.MemberAuth;
 import kr.touroot.global.exception.dto.ExceptionResponse;
 import kr.touroot.travelogue.dto.request.TravelogueRequest;
+import kr.touroot.travelogue.dto.request.TravelogueSearchRequest;
 import kr.touroot.travelogue.dto.response.TravelogueResponse;
 import kr.touroot.travelogue.dto.response.TravelogueSimpleResponse;
 import kr.touroot.travelogue.service.TravelogueFacadeService;
@@ -24,19 +23,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "여행기")
 @RequiredArgsConstructor
-@Validated
 @RestController
 @RequestMapping("api/v1/travelogues")
 public class TravelogueController {
@@ -123,12 +119,10 @@ public class TravelogueController {
             @Parameter(hidden = true)
             @PageableDefault(size = 5, sort = "id", direction = Direction.DESC)
             Pageable pageable,
-            @RequestParam
-            @NotBlank(message = "검색어는 2글자 이상이어야 합니다.")
-            @Size(min = 2, message = "검색어는 2글자 이상이어야 합니다.")
-            String keyword
+            @Valid
+            TravelogueSearchRequest searchRequest
     ) {
-        return ResponseEntity.ok(travelogueFacadeService.findSimpleTraveloguesByKeyword(pageable, keyword));
+        return ResponseEntity.ok(travelogueFacadeService.findSimpleTravelogues(pageable, searchRequest));
     }
 
     @Operation(summary = "여행기 삭제")
