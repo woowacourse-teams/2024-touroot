26ef51c24a372759ce6af07458b04b7389bdb883
hangillee <skfcb10@naver.com>
2024-08-14 10:38:45 +0900

    refactor: 필드 final 추가

--- backend/src/main/java/kr/touroot/global/config/QueryDslConfig.java
+++ backend/src/main/java/kr/touroot/global/config/QueryDslConfig.java
@@ -3,14 +3,16 @@ package kr.touroot.global.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@RequiredArgsConstructor
 @Configuration
 public class QueryDslConfig {
 
     @PersistenceContext
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
