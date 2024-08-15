df12114c5ee7667e55ad88161e39a04e74d0ceae
손진영 <sm099@naver.com>
2024-08-15 16:48:54 +0900

    feat: TravelPlanTodoItem 컴포넌트 구현

---
+++ frontend/src/components/pages/travelPlanDetail/TravelPlanTodoItem/TravelPlanTodoItem.styled.ts
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const TodoListItemContainer = styled.li`
+  display: flex;
+  gap: 0.8rem;
+`;
---
+++ frontend/src/components/pages/travelPlanDetail/TravelPlanTodoItem/TravelPlanTodoItem.tsx
@@ -0,0 +1,36 @@
+import { useLocation } from "react-router-dom";
+
+import { TravelPlanTodo } from "@type/domain/travelPlan";
+
+import { usePatchTravelPlanTodo } from "@queries/usePatchTravelPlanTodo";
+
+import { Checkbox, Text } from "@components/common";
+
+import { extractId } from "@utils/extractId";
+import { isUUID } from "@utils/uuid";
+
+import * as S from "./TravelPlanTodoItem.styled";
+
+const TravelPlanTodoItem = ({ todo }: { todo: TravelPlanTodo }) => {
+  const location = useLocation();
+  const id = extractId(location.pathname);
+  const { mutate: onUpdateTodoStatus } = usePatchTravelPlanTodo(id);
+
+  return (
+    <S.TodoListItemContainer key={todo.id}>
+      <Checkbox
+        onClick={() => {
+          if (!isUUID(id))
+            onUpdateTodoStatus({
+              todoId: todo.id as string,
+              checked: todo.checked as boolean,
+            });
+        }}
+        isChecked={todo.checked ?? false}
+      />
+      <Text textType="detail">{todo.content}</Text>
+    </S.TodoListItemContainer>
+  );
+};
+
+export default TravelPlanTodoItem;
