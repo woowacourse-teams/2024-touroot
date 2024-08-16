cdc384699e1749fb044a2c98dc8d64b36d10f046
river <130737187+0jenn0@users.noreply.github.com>
2024-08-16 13:06:06 +0900

    feat: 검색 결과 페이지 구현

---
+++ frontend/src/components/pages/search/SearchPage.styled.ts
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1.6rem;
+
+  gap: 8px;
+
+  h1 {
+    ${({ theme }) => theme.typography.mobile.title};
+  }
+
+  p {
+    color: ${(props) => props.theme.colors.text.secondary};
+    font-size: 1.2rem;
+  }
+`;
+
+export const MainPageTraveloguesList = styled.ul`
+  display: flex;
+  flex-direction: column;
+
+  gap: 4rem;
+`;
+
+export const MainPageContentContainer = styled.div`
+  padding-top: 1.6rem;
+`;
