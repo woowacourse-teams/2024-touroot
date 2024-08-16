396d2c88a706f8887a8c96d27e2f63f0c8608c93
river <130737187+0jenn0@users.noreply.github.com>
2024-08-16 15:55:01 +0900

    feat: AppLayout에서 Header를 동적으로 추가

--- frontend/src/components/layout/AppLayout/AppLayout.tsx
+++ frontend/src/components/layout/AppLayout/AppLayout.tsx
@@ -1,13 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
-import { Header } from "@components/common";
+import HomeHeader from "@components/common/Header/HomeHeader/HomeHeader";
+import LogoHeader from "@components/common/Header/LogoHeader/LogoHeader";
+import SearchHeader from "@components/common/Header/SearchHeader/SearchHeader";
+
+import { ROUTE_PATHS_MAP } from "@constants/route";
 
 import * as S from "./AppLayout.styled";
 
 const AppLayout = () => {
+  const location = useLocation();
+  const pathName = location.pathname;
+
+  const encodedKeyword =
+    location.pathname.split("/").length > 2 ? location.pathname.split("/").pop() : "";
+  const receivedKeyword = encodedKeyword ? decodeURIComponent(encodedKeyword) : "";
+
+  const getHeader = (pathName: string) => {
+    if (pathName === ROUTE_PATHS_MAP.root) return <LogoHeader />;
+    if (pathName === ROUTE_PATHS_MAP.searchMain) return <SearchHeader />;
+    if (receivedKeyword && pathName.includes(ROUTE_PATHS_MAP.search().split("/")[1]))
+      return <SearchHeader />;
+    return <HomeHeader />;
+  };
+
   return (
     <>
-      <Header />
+      {getHeader(pathName)}
       <S.OutletContainer>
         <Outlet />
       </S.OutletContainer>
