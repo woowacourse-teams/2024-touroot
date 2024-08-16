0861f1e45960936fd712ab4234475c58de39c95b
libienz <leekh10195@gmail.com>
2024-08-16 13:54:41 +0900

    refactor: 사용하지 않는 메서드 제거 개선

--- backend/src/main/java/kr/touroot/travelplan/repository/PlaceTodoRepository.java
+++ backend/src/main/java/kr/touroot/travelplan/repository/PlaceTodoRepository.java
@@ -1,28 +1,14 @@
 package kr.touroot.travelplan.repository;
 
 import java.util.List;
-import java.util.Optional;
-import kr.touroot.member.domain.Member;
 import kr.touroot.travelplan.domain.TravelPlaceTodo;
 import kr.touroot.travelplan.domain.TravelPlan;
 import kr.touroot.travelplan.domain.TravelPlanPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface PlaceTodoRepository extends JpaRepository<TravelPlaceTodo, Long> {
 
     List<TravelPlaceTodo> findByTravelPlanPlace(TravelPlanPlace travelPlanPlace);
-
-    @Query("""
-            SELECT m FROM Member m
-                JOIN TravelPlan p ON p.author = m
-                JOIN TravelPlanDay d ON d.plan = p
-                JOIN TravelPlanPlace pl ON pl.day = d
-                JOIN TravelPlaceTodo t ON t.travelPlanPlace = pl
-            WHERE t = :placeTodo
-            """)
-    Optional<Member> findOwnerOf(@Param("placeTodo") TravelPlaceTodo placeTodo);
 
     void deleteByTravelPlanPlaceDayPlan(TravelPlan travelPlan);
 }
