325d2fe00bbc1fd954181859940f44a5a06ac4ac
river <130737187+0jenn0@users.noreply.github.com>
2024-08-16 15:48:14 +0900

    feat: HomeHeader 구현

---
+++ frontend/src/components/common/Header/HomeHeader/HomeHeader.tsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
+import IconButton from "@components/common/IconButton/IconButton";
+
+import { ROUTE_PATHS_MAP } from "@constants/route";
+
+import Header from "../Header";
+
+const HomeHeader = () => {
+  const navigation = useNavigate();
+  return (
+    <Header
+      rightContent={
+        <IconButton
+          iconType="home-icon"
+          size="20"
+          onClick={() => navigation(ROUTE_PATHS_MAP.root)}
+        />
+      }
+      useHamburger
+    />
+  );
+};
+
+export default HomeHeader;
