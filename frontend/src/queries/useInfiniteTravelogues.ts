ff87309eef1d5ff5d94bbbd6c3b9caa2c1892c7f
simhorim <hori5020@gmail.com>
2024-08-16 22:19:31 +0900

    feat(useInfiniteTravelogues): 필터링 기능 추가

--- frontend/src/queries/useInfiniteTravelogues.ts
+++ frontend/src/queries/useInfiniteTravelogues.ts
@@ -5,24 +5,34 @@ import { client } from "@apis/client";
 import { API_ENDPOINT_MAP } from "@constants/endpoint";
 import { QUERY_KEYS_MAP } from "@constants/queryKey";
 
-export const getTravelogues = async ({ page, size }: { page: number; size: number }) => {
+export const getTravelogues = async ({
+  page,
+  size,
+  tagFilter,
+}: {
+  page: number;
+  size: number;
+  tagFilter?: string;
+}) => {
   const response = await client.get(API_ENDPOINT_MAP.travelogues, {
-    params: { page, size },
+    params: { page, size, "tag-filter": tagFilter },
   });
 
   return response.data;
 };
 
-const useInfiniteTravelogues = () => {
+const useInfiniteTravelogues = (selectedTagIDs: number[]) => {
   const INITIAL_PAGE = 0;
   const DATA_LOAD_COUNT = 5;
 
   const { data, status, error, fetchNextPage, isFetchingNextPage, hasNextPage } = useInfiniteQuery({
-    queryKey: QUERY_KEYS_MAP.travelogue.all,
+    queryKey: [QUERY_KEYS_MAP.travelogue.all, ...selectedTagIDs],
     queryFn: ({ pageParam = INITIAL_PAGE }) => {
       const page = pageParam;
       const size = DATA_LOAD_COUNT;
-      return getTravelogues({ page, size });
+      const tagFilter = [...selectedTagIDs].join(",");
+
+      return tagFilter ? getTravelogues({ page, size, tagFilter }) : getTravelogues({ page, size });
     },
     initialPageParam: 0,
     getNextPageParam: (lastPage, allPages) => {
