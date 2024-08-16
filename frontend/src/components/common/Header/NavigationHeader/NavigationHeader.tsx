08acd6d42ce70e97f166df736c606ee9606466f0
river <130737187+0jenn0@users.noreply.github.com>
2024-08-16 15:48:35 +0900

    feat: NavigationHeader 구현

---
+++ frontend/src/components/common/Header/NavigationHeader/NavigationHeader.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+import IconButton from "@components/common/IconButton/IconButton";
+
+import { ROUTE_PATHS_MAP } from "@constants/route";
+
+import Header from "../Header";
+
+const NavigationHeader = () => {
+  const navigation = useNavigate();
+
+  return (
+    <Header
+      rightContent={
+        <IconButton
+          iconType="search-icon"
+          size="18"
+          onClick={() => navigation(ROUTE_PATHS_MAP.searchMain)}
+        />
+      }
+      useHamburger
+    />
+  );
+};
+
+export default NavigationHeader;
