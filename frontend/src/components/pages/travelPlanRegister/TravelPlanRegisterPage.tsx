9bfe35c7bbb001501c1b04acf25e3d433cb1d479
simhorim <hori5020@gmail.com>
2024-08-18 01:55:45 +0900

    refactor(TravelPlanRegisterPage): 상수화에 따른 수정

--- frontend/src/components/pages/travelPlanRegister/TravelPlanRegisterPage.tsx
+++ frontend/src/components/pages/travelPlanRegister/TravelPlanRegisterPage.tsx
@@ -22,13 +22,11 @@ import { useTravelPlanDays } from "@hooks/pages/useTravelPlanDays";
 import useLeadingDebounce from "@hooks/useLeadingDebounce";
 import useUser from "@hooks/useUser";
 
+import { CONDITIONS_MAP } from "@constants/condition";
 import { ERROR_MESSAGE_MAP } from "@constants/errorMessage";
 import { ROUTE_PATHS_MAP } from "@constants/route";
 
 import * as S from "./TravelPlanRegisterPage.styled";
-
-const MIN_TITLE_LENGTH = 0;
-const MAX_TITLE_LENGTH = 20;
 
 const TravelPlanRegisterPage = () => {
   const { transformDetail, saveTransformDetail } = useTravelTransformDetailContext();
@@ -47,7 +45,10 @@ const TravelPlanRegisterPage = () => {
   } = useTravelPlanDays(transformDetail?.days ?? []);
 
   const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const title = e.target.value.slice(MIN_TITLE_LENGTH, MAX_TITLE_LENGTH);
+    const title = e.target.value.slice(
+      CONDITIONS_MAP.title.minLength,
+      CONDITIONS_MAP.title.maxLength,
+    );
     setTitle(title);
   };
 
@@ -118,11 +119,11 @@ const TravelPlanRegisterPage = () => {
         <PageInfo mainText="여행 계획 등록" />
         <Input
           value={title}
-          maxLength={MAX_TITLE_LENGTH}
+          maxLength={CONDITIONS_MAP.title.maxLength}
           label="제목"
           placeholder="여행 계획 제목을 입력해주세요"
           count={title.length}
-          maxCount={MAX_TITLE_LENGTH}
+          maxCount={CONDITIONS_MAP.title.maxLength}
           onChange={handleChangeTitle}
         />
         <S.StartDateContainer>
