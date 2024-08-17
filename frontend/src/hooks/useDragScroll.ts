8828ace8e44ebe239b23152f8c17a7054466b52e
simhorim <hori5020@gmail.com>
2024-08-18 01:51:38 +0900

    refactor(useDragScroll): 타입 유연성을 위해 제네릭으로 수정

--- frontend/src/hooks/useDragScroll.ts
+++ frontend/src/hooks/useDragScroll.ts
@@ -1,12 +1,12 @@
 import { useCallback, useRef, useState } from "react";
 
-export const useDragScroll = () => {
-  const scrollRef = useRef<HTMLUListElement>(null);
+export const useDragScroll = <T extends HTMLElement>() => {
+  const scrollRef = useRef<T>(null);
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
 
-  const onMouseDown = useCallback((e: React.MouseEvent<HTMLUListElement>) => {
+  const onMouseDown = useCallback((e: React.MouseEvent<T>) => {
     if (!scrollRef.current) return;
     setIsDragging(true);
     setStartX(e.pageX - scrollRef.current.offsetLeft);
@@ -18,7 +18,7 @@ export const useDragScroll = () => {
   }, []);
 
   const onMouseMove = useCallback(
-    (e: React.MouseEvent<HTMLUListElement>) => {
+    (e: React.MouseEvent<T>) => {
       if (!isDragging || !scrollRef.current) return;
       e.preventDefault();
       const x = e.pageX - scrollRef.current.offsetLeft;
