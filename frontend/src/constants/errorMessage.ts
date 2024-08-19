59749e022b61ad7ddaf2ed6a94ebd163d2cdedda
simhorim <hori5020@gmail.com>
2024-08-19 18:01:59 +0900

    feat(errorMessage): errorMessage 추가

--- frontend/src/constants/errorMessage.ts
+++ frontend/src/constants/errorMessage.ts
@@ -4,6 +4,8 @@ export const ERROR_MESSAGE_MAP = {
     expiredToken: "이미 만료된 토큰입니다.",
     travelPlanOnlyWriter: "여행 계획 조회는 작성자만 가능합니다.",
     travelogueEditOnlyWriter: "여행기 수정은 작성자만 가능합니다.",
+    travelPlanEditOnlyWriter: "여행 계획 수정은 작성자만 가능합니다.",
+    travelPlanNotFound: "존재하지 않는 여행 계획입니다.",
   },
   loginFailed: "로그인에 실패하였습니다. 다시 시도해주세요!",
   imageUpload: "사진은 10장 이하만 업로드 해주세요.",
