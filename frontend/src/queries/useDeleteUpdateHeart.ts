71107c64c66c16953e2d4190438a4ae773d8e06d
손진영 <sm099@naver.com>
2024-08-21 18:04:19 +0900

    fix: 좋아요를 누르면 로그인 alert가 2번 뜨는 문제 해결
    좋아요를 눌렀을 때 interceptor의 alert와 onError 콜백의 alert가 겹쳐 2번 alert 뜨는 문제 해결

--- frontend/src/queries/useDeleteUpdateHeart.ts
+++ frontend/src/queries/useDeleteUpdateHeart.ts
@@ -9,6 +9,7 @@ import ApiError from "@apis/ApiError";
 import { authClient } from "@apis/client";
 
 import { API_ENDPOINT_MAP } from "@constants/endpoint";
+import { ERROR_MESSAGE_MAP } from "@constants/errorMessage";
 import { QUERY_KEYS_MAP } from "@constants/queryKey";
 
 export const useDeleteUpdateHeart = () => {
@@ -26,6 +27,10 @@ export const useDeleteUpdateHeart = () => {
       });
     },
     onError: (error) => {
+      if (error instanceof ApiError && error.message === ERROR_MESSAGE_MAP.api.login) {
+        return;
+      }
+
       alert(error.message);
     },
   });
