dea8660e9ac7c93a4f07ae4927b9b4174ad94e83
river <130737187+0jenn0@users.noreply.github.com>
2024-08-22 01:13:43 +0900

    fix(TravelPlanRegisterPage): import되어있지 않은 컴포넌트 import

--- frontend/src/components/pages/travelPlanRegister/TravelPlanRegisterPage.tsx
+++ frontend/src/components/pages/travelPlanRegister/TravelPlanRegisterPage.tsx
@@ -14,6 +14,7 @@ import {
   Input,
   ModalBottomSheet,
   PageInfo,
+  Text,
   TextField,
 } from "@components/common";
 import TravelPlanDayAccordion from "@components/pages/travelPlanRegister/TravelPlanDayAccordion/TravelPlanDayAccordion";
