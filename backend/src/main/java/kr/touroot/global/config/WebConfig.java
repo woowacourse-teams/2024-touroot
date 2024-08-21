f97861487af7545a70fbaf7416a94f7ad4454f5f
eunjungL <62099953+eunjungL@users.noreply.github.com>
2024-08-21 10:55:09 +0900

    [Fix] - Prod CORS 허용 및 태그 필터링 정렬 순서 변경 (#365)
    * refactor: allowed origins application.yml에서 관리하게 변경
    
    * fix: 여행기 태그 필터링 정렬 수정

--- backend/src/main/java/kr/touroot/global/config/WebConfig.java
+++ backend/src/main/java/kr/touroot/global/config/WebConfig.java
@@ -2,17 +2,25 @@ package kr.touroot.global.config;
 
 import java.util.List;
 import kr.touroot.global.auth.MemberAuthMethodArgumentResolver;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final MemberAuthMethodArgumentResolver memberAuthMethodArgumentResolver;
+    private final String[] allowedOrigins;
+
+    public WebConfig(
+            MemberAuthMethodArgumentResolver memberAuthMethodArgumentResolver,
+            @Value("${cors.allowed-origins}") String[] allowedOrigins
+    ) {
+        this.memberAuthMethodArgumentResolver = memberAuthMethodArgumentResolver;
+        this.allowedOrigins = allowedOrigins;
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
@@ -22,7 +30,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://dev.touroot.kr")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS");
     }
 }
