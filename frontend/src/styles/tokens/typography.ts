d594224a4917d4f0fca26a555825752037eee726
choi river <130737187+0jenn0@users.noreply.github.com>
2024-08-23 10:25:12 +0900

    [Fix] - line height,shareModal 수정 (#407)
    * fix(shareModal): 잘못된 텍스트 수정
    
    * style(typography): line-height 수정

--- frontend/src/styles/tokens/typography.ts
+++ frontend/src/styles/tokens/typography.ts
@@ -16,37 +16,37 @@ export const TYPOGRAPHY = {
     heading: `
       font-weight: 700;
       font-size: ${FONT_SIZES.mobile.xxxl};
-      line-height: 1.5rem;
+      line-height: ${FONT_SIZES.mobile.xxxl};
     `,
     title: `
       font-weight: 700;
       font-size: ${FONT_SIZES.mobile.xl};
-      line-height: 1.5rem;
+      line-height: ${FONT_SIZES.mobile.xl};
     `,
     subTitle: `
       font-weight: 700;
       font-size: ${FONT_SIZES.mobile.l};
-      line-height: 1.5rem;
+      line-height: ${FONT_SIZES.mobile.l};
     `,
     body: `
       font-weight: 400;
       font-size: ${FONT_SIZES.mobile.s};
-      line-height: 1.5rem;
+      line-height:  ${FONT_SIZES.mobile.s};
     `,
     bodyBold: `
       font-weight: 700;
       font-size: ${FONT_SIZES.mobile.s};
-      line-height: 1.5rem;
+      line-height: ${FONT_SIZES.mobile.s};
     `,
     detail: `
       font-weight: 400;
       font-size: ${FONT_SIZES.mobile.xxs};
-      line-height: 1.5rem;
+      line-height:  ${FONT_SIZES.mobile.xxs};
     `,
     detailBold: `
       font-weight: 700;
       font-size: ${FONT_SIZES.mobile.xxs};
-      line-height: 1.5rem;
+      line-height:${FONT_SIZES.mobile.xxs};
     `,
   },
 } as const;
