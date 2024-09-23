52d496b6d8f8fe5d71799eb68d8d6c1368cd2956
libienz <leekh10195@gmail.com>
2024-09-23 17:43:23 +0900

    fix: LoginService 트랜잭셔널 검토

--- backend/src/main/java/kr/touroot/global/config/DynamicRoutingDataSource.java
+++ backend/src/main/java/kr/touroot/global/config/DynamicRoutingDataSource.java
@@ -1,15 +1,19 @@
 package kr.touroot.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 public class DynamicRoutingDataSource extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
         if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            log.info("난 리더~ 내 말을 따르지 않는 사람들 숙청하지~");
             return DataSourceConfig.READER;
         }
+        log.info("난 라이터~ 담배에 불을 붙일 수 있지~");
         return DataSourceConfig.WRITER;
     }
 }
