a4e233242942362c87eef8cbed0f138895e01874
손진영 <sm099@naver.com>
2024-09-24 19:28:52 +0900

    refactor: 코드 스플리팅 적용

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,20 +1,62 @@
+import { Suspense, lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
+import { css } from "@emotion/react";
+
+import { Text } from "@components/common";
+import Spinner from "@components/common/Spinner/Spinner";
 import AppLayout from "@components/layout/AppLayout/AppLayout";
-import KakaoCallbackPage from "@components/pages/login/KakaoCallbackPage";
-import LoginPage from "@components/pages/login/LoginPage";
-import MainPage from "@components/pages/main/MainPage";
-import MyPage from "@components/pages/my/MyPage";
-import NotFoundPage from "@components/pages/notFound/NotFoundPage";
-import SearchPage from "@components/pages/search/SearchPage";
-import TravelPlanDetailPage from "@components/pages/travelPlanDetail/TravelPlanDetailPage";
-import TravelPlanEditPage from "@components/pages/travelPlanEdit/TravelPlanEditPage";
-import TravelPlanRegisterPage from "@components/pages/travelPlanRegister/TravelPlanRegisterPage";
-import TravelogueDetailPage from "@components/pages/travelogueDetail/TravelogueDetailPage";
-import TravelogueEditPage from "@components/pages/travelogueEdit/TravelogueEditPage";
-import TravelogueRegisterPage from "@components/pages/travelogueRegister/TravelogueRegisterPage";
 
 import { ROUTE_PATHS_MAP } from "./constants/route";
+
+const MainPage = lazy(() => import("@components/pages/main/MainPage"));
+const LoginPage = lazy(() => import("@components/pages/login/LoginPage"));
+const KakaoCallbackPage = lazy(() => import("@components/pages/login/KakaoCallbackPage"));
+const TravelogueDetailPage = lazy(
+  () => import("@components/pages/travelogueDetail/TravelogueDetailPage"),
+);
+const TravelogueRegisterPage = lazy(
+  () => import("@components/pages/travelogueRegister/TravelogueRegisterPage"),
+);
+const TravelogueEditPage = lazy(
+  () => import("@components/pages/travelogueEdit/TravelogueEditPage"),
+);
+const TravelPlanDetailPage = lazy(
+  () => import("@components/pages/travelPlanDetail/TravelPlanDetailPage"),
+);
+const TravelPlanRegisterPage = lazy(
+  () => import("@components/pages/travelPlanRegister/TravelPlanRegisterPage"),
+);
+const TravelPlanEditPage = lazy(
+  () => import("@components/pages/travelPlanEdit/TravelPlanEditPage"),
+);
+const MyPage = lazy(() => import("@components/pages/my/MyPage"));
+const SearchPage = lazy(() => import("@components/pages/search/SearchPage"));
+const NotFoundPage = lazy(() => import("@components/pages/notFound/NotFoundPage"));
+
+const lazyLoadingFallbackStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  max-width: 48rem;
+  gap: 1rem;
+`;
+
+const withLazyLoading = (Component: React.ComponentType) => (
+  <Suspense
+    fallback={
+      <div css={lazyLoadingFallbackStyle}>
+        <Spinner />
+        <Text textType="bodyBold">로딩 중이에요. 잠시만 기다려주세요 :)</Text>
+      </div>
+    }
+  >
+    <Component />
+  </Suspense>
+);
 
 export const router = createBrowserRouter([
   {
@@ -23,56 +65,55 @@ export const router = createBrowserRouter([
     children: [
       {
         path: ROUTE_PATHS_MAP.root,
-        element: <MainPage />,
+        element: withLazyLoading(MainPage),
       },
       {
         path: ROUTE_PATHS_MAP.login,
-        element: <LoginPage />,
+        element: withLazyLoading(LoginPage),
       },
-
       {
         path: ROUTE_PATHS_MAP.loginCallback,
-        element: <KakaoCallbackPage />,
+        element: withLazyLoading(KakaoCallbackPage),
       },
       {
         path: ROUTE_PATHS_MAP.travelogue(),
-        element: <TravelogueDetailPage />,
+        element: withLazyLoading(TravelogueDetailPage),
       },
       {
         path: ROUTE_PATHS_MAP.travelogueRegister,
-        element: <TravelogueRegisterPage />,
+        element: withLazyLoading(TravelogueRegisterPage),
       },
       {
         path: ROUTE_PATHS_MAP.travelogueEdit(),
-        element: <TravelogueEditPage />,
+        element: withLazyLoading(TravelogueEditPage),
       },
       {
         path: ROUTE_PATHS_MAP.travelPlan(),
-        element: <TravelPlanDetailPage />,
+        element: withLazyLoading(TravelPlanDetailPage),
       },
       {
         path: ROUTE_PATHS_MAP.travelPlanRegister,
-        element: <TravelPlanRegisterPage />,
+        element: withLazyLoading(TravelPlanRegisterPage),
       },
       {
         path: ROUTE_PATHS_MAP.travelPlanEdit(),
-        element: <TravelPlanEditPage />,
+        element: withLazyLoading(TravelPlanEditPage),
       },
       {
         path: ROUTE_PATHS_MAP.my,
-        element: <MyPage />,
+        element: withLazyLoading(MyPage),
       },
       {
         path: ROUTE_PATHS_MAP.searchMain,
-        element: <SearchPage />,
+        element: withLazyLoading(SearchPage),
       },
       {
         path: ROUTE_PATHS_MAP.search(),
-        element: <SearchPage />,
+        element: withLazyLoading(SearchPage),
       },
       {
         path: "*",
-        element: <NotFoundPage />,
+        element: withLazyLoading(NotFoundPage),
       },
     ],
   },
