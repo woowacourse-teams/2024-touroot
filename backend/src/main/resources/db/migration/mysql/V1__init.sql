7ad25be68eabc83dedf62f65b7bf24a27c40665c
리비 <Leekh10195@gmail.com>
2024-09-25 21:02:07 +0900

    [Refactor] - Place 엔티티 역정규화 (#443)
    * refactor: 여행기 장소 엔티티에서 Place 엔티티 제거 개선
    
    * refactor: 여행 계획 장소 엔티티에서 Place 엔티티 제거 개선
    
    * test: TraveloguePlaceTest 누락된 메서드 테스트 추가
    
    * refactor: 사용하지 않는 Place 엔티티 제거
    
    * refactor: 위도 경도 정규식 상수화
    
    * feat: 위도 경도 값 타입 구현
    
    * test: GeographicalCoordinateTest 작성
    
    * test: GeographicalCoordinateTest 경계값 테스트 추가
    
    * refactor: 위경도 값타입을 사용하도록 여행기, 여행계획 엔티티 개선
    
    * feat: place 엔티티 삭제한 버전의 sql 스크립트 추가
    
    * fix: V1 flyway 스크립트 제약조건 이름 설정하도록 수정
    
    * fix: V2 flyway 스크립트 문법 오류 수정
    
    * fix: 지명 85자 이하로 명세 수정
    
    * refactor: position Coord 용어 position으로 통일 개
    
    * refactor: coordinate 패키지 position 으로 이름 변경
    
    * feat: V2 flywayscript 수정 - member 테이블의 닉네임 기반 인덱스 생성
    
    * feat: V2 flywayscript 수정 - member 테이블의 닉네임 기반 인덱스 삭제
    
    * fix: 테스트 디스플레이 네임 수정

--- backend/src/main/resources/db/migration/mysql/V1__init.sql
+++ backend/src/main/resources/db/migration/mysql/V1__init.sql
@@ -1,25 +1,24 @@
--- 테이블 생성
 CREATE TABLE member
 (
-    created_at        TIMESTAMP(6) NOT NULL,
-    deleted_at        TIMESTAMP(6),
-    id                BIGINT AUTO_INCREMENT,
+    created_at        DATETIME(6) NOT NULL,
+    deleted_at        DATETIME(6),
+    id                BIGINT NOT NULL AUTO_INCREMENT,
     kakao_id          BIGINT,
-    modified_at       TIMESTAMP(6),
+    modified_at       DATETIME(6),
     email             VARCHAR(255),
     nickname          VARCHAR(255) NOT NULL,
     password          VARCHAR(255),
     profile_image_url VARCHAR(255) NOT NULL,
-    login_type        ENUM('DEFAULT','KAKAO') NOT NULL,
+    login_type        ENUM ('DEFAULT','KAKAO') NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE place
 (
-    created_at      TIMESTAMP(6) NOT NULL,
-    deleted_at      TIMESTAMP(6),
-    id              BIGINT AUTO_INCREMENT,
-    modified_at     TIMESTAMP(6),
+    created_at      DATETIME(6) NOT NULL,
+    deleted_at      DATETIME(6),
+    id              BIGINT NOT NULL AUTO_INCREMENT,
+    modified_at     DATETIME(6),
     google_place_id VARCHAR(255),
     latitude        VARCHAR(255) NOT NULL,
     longitude       VARCHAR(255) NOT NULL,
@@ -29,141 +28,172 @@ CREATE TABLE place
 
 CREATE TABLE tag
 (
-    id  BIGINT AUTO_INCREMENT,
-    tag VARCHAR(255) NOT NULL UNIQUE,
+    id  BIGINT NOT NULL AUTO_INCREMENT,
+    tag VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE travelogue
+(
+    author_id   BIGINT NOT NULL,
+    created_at  DATETIME(6) NOT NULL,
+    deleted_at  DATETIME(6),
+    id          BIGINT NOT NULL AUTO_INCREMENT,
+    modified_at DATETIME(6),
+    title       VARCHAR(20) NOT NULL,
+    thumbnail   VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE travelogue_day
+(
+    day_order     INT NOT NULL,
+    created_at    DATETIME(6) NOT NULL,
+    deleted_at    DATETIME(6),
+    id            BIGINT NOT NULL AUTO_INCREMENT,
+    modified_at   DATETIME(6),
+    travelogue_id BIGINT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE travelogue_like
+(
+    id            BIGINT NOT NULL AUTO_INCREMENT,
+    liker_id      BIGINT NOT NULL,
+    travelogue_id BIGINT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE travelogue_photo
+(
+    photo_order         INT NOT NULL,
+    created_at          DATETIME(6) NOT NULL,
+    deleted_at          DATETIME(6),
+    id                  BIGINT NOT NULL AUTO_INCREMENT,
+    modified_at         DATETIME(6),
+    travelogue_place_id BIGINT NOT NULL,
+    photo_key           VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE travelogue_place
+(
+    place_order       INT NOT NULL,
+    created_at        DATETIME(6) NOT NULL,
+    deleted_at        DATETIME(6),
+    id                BIGINT NOT NULL AUTO_INCREMENT,
+    modified_at       DATETIME(6),
+    place_id          BIGINT NOT NULL,
+    travelogue_day_id BIGINT NOT NULL,
+    description       VARCHAR(300),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE travelogue_tag
+(
+    id            BIGINT NOT NULL AUTO_INCREMENT,
+    tag_id        BIGINT NOT NULL,
+    travelogue_id BIGINT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE travel_place_todo
+(
+    is_checked           BIT NOT NULL,
+    todo_order           INT NOT NULL,
+    created_at           DATETIME(6) NOT NULL,
+    deleted_at           DATETIME(6),
+    id                   BIGINT NOT NULL AUTO_INCREMENT,
+    modified_at          DATETIME(6),
+    travel_plan_place_id BIGINT NOT NULL,
+    content              VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE travel_plan
 (
-    start_date  DATE         NOT NULL,
-    author_id   BIGINT       NOT NULL,
-    created_at  TIMESTAMP(6) NOT NULL,
-    deleted_at  TIMESTAMP(6),
-    id          BIGINT AUTO_INCREMENT,
-    modified_at TIMESTAMP(6),
+    start_date  DATE NOT NULL,
+    author_id   BIGINT NOT NULL,
+    created_at  DATETIME(6) NOT NULL,
+    deleted_at  DATETIME(6),
+    id          BIGINT NOT NULL AUTO_INCREMENT,
+    modified_at DATETIME(6),
     share_key   BINARY(16) NOT NULL,
     title       VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (author_id) REFERENCES member (id)
-);
-
-CREATE TABLE travelogue
-(
-    author_id   BIGINT       NOT NULL,
-    created_at  TIMESTAMP(6) NOT NULL,
-    deleted_at  TIMESTAMP(6),
-    id          BIGINT AUTO_INCREMENT,
-    modified_at TIMESTAMP(6),
-    title       VARCHAR(20)  NOT NULL,
-    thumbnail   VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (author_id) REFERENCES member (id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE travel_plan_day
 (
-    plan_day_order INT          NOT NULL,
-    created_at     TIMESTAMP(6) NOT NULL,
-    deleted_at     TIMESTAMP(6),
-    id             BIGINT AUTO_INCREMENT,
-    modified_at    TIMESTAMP(6),
-    plan_id        BIGINT       NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (plan_id) REFERENCES travel_plan (id)
+    plan_day_order INT NOT NULL,
+    created_at     DATETIME(6) NOT NULL,
+    deleted_at     DATETIME(6),
+    id             BIGINT NOT NULL AUTO_INCREMENT,
+    modified_at    DATETIME(6),
+    plan_id        BIGINT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE travel_plan_place
 (
-    plan_place_order INT          NOT NULL,
-    created_at       TIMESTAMP(6) NOT NULL,
-    deleted_at       TIMESTAMP(6),
-    id               BIGINT AUTO_INCREMENT,
-    modified_at      TIMESTAMP(6),
-    place_id         BIGINT       NOT NULL,
-    plan_day_id      BIGINT       NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (place_id) REFERENCES place (id),
-    FOREIGN KEY (plan_day_id) REFERENCES travel_plan_day (id)
-);
-
-CREATE TABLE travel_place_todo
-(
-    is_checked           BOOLEAN      NOT NULL,
-    todo_order           INT          NOT NULL,
-    created_at           TIMESTAMP(6) NOT NULL,
-    deleted_at           TIMESTAMP(6),
-    id                   BIGINT AUTO_INCREMENT,
-    modified_at          TIMESTAMP(6),
-    travel_plan_place_id BIGINT       NOT NULL,
-    content              VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (travel_plan_place_id) REFERENCES travel_plan_place (id)
-);
-
-CREATE TABLE travelogue_day
-(
-    day_order     INT          NOT NULL,
-    created_at    TIMESTAMP(6) NOT NULL,
-    deleted_at    TIMESTAMP(6),
-    id            BIGINT AUTO_INCREMENT,
-    modified_at   TIMESTAMP(6),
-    travelogue_id BIGINT       NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (travelogue_id) REFERENCES travelogue (id)
-);
-
-CREATE TABLE travelogue_place
-(
-    place_order       INT          NOT NULL,
-    created_at        TIMESTAMP(6) NOT NULL,
-    deleted_at        TIMESTAMP(6),
-    id                BIGINT AUTO_INCREMENT,
-    modified_at       TIMESTAMP(6),
-    place_id          BIGINT       NOT NULL,
-    travelogue_day_id BIGINT       NOT NULL,
-    descriptiON      VARCHAR(300),
-    PRIMARY KEY (id),
-    FOREIGN KEY (place_id) REFERENCES place (id),
-    FOREIGN KEY (travelogue_day_id) REFERENCES travelogue_day (id)
-);
-
-CREATE TABLE travelogue_photo
-(
-    photo_order         INT          NOT NULL,
-    created_at          TIMESTAMP(6) NOT NULL,
-    deleted_at          TIMESTAMP(6),
-    id                  BIGINT AUTO_INCREMENT,
-    modified_at         TIMESTAMP(6),
-    travelogue_place_id BIGINT       NOT NULL,
-    photo_key           VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (travelogue_place_id) REFERENCES travelogue_place (id)
-);
-
-CREATE TABLE travelogue_tag
-(
-    id            BIGINT AUTO_INCREMENT,
-    tag_id        BIGINT NOT NULL,
-    travelogue_id BIGINT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (tag_id) REFERENCES tag (id),
-    FOREIGN KEY (travelogue_id) REFERENCES travelogue (id)
-);
-
-CREATE TABLE travelogue_like
-(
-    id            BIGINT AUTO_INCREMENT,
-    liker_id      BIGINT NOT NULL,
-    travelogue_id BIGINT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY (travelogue_id, liker_id),
-    FOREIGN KEY (liker_id) REFERENCES member (id),
-    FOREIGN KEY (travelogue_id) REFERENCES travelogue (id)
+    plan_place_order INT NOT NULL,
+    created_at       DATETIME(6) NOT NULL,
+    deleted_at       DATETIME(6),
+    id               BIGINT NOT NULL AUTO_INCREMENT,
+    modified_at      DATETIME(6),
+    place_id         BIGINT NOT NULL,
+    plan_day_id      BIGINT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE INDEX member_kakao_id_idx ON member (kakao_id);
-
 CREATE INDEX place_name_latitude_longitude_idx ON place (name, latitude, longitude);
-
 CREATE INDEX travel_plan_share_key_idx ON travel_plan (share_key);
+
+ALTER TABLE tag
+    ADD CONSTRAINT uk_tag UNIQUE (tag);
+
+ALTER TABLE travelogue_like
+    ADD CONSTRAINT uk_travelogue_like_travelogue_liker UNIQUE (travelogue_id, liker_id);
+
+ALTER TABLE travelogue
+    ADD CONSTRAINT fk_travelogue_author_id FOREIGN KEY (author_id) REFERENCES member (id);
+
+ALTER TABLE travelogue_day
+    ADD CONSTRAINT fk_travelogue_day_travelogue_id FOREIGN KEY (travelogue_id) REFERENCES travelogue (id);
+
+ALTER TABLE travelogue_like
+    ADD CONSTRAINT fk_travelogue_like_liker_id FOREIGN KEY (liker_id) REFERENCES member (id);
+
+ALTER TABLE travelogue_like
+    ADD CONSTRAINT fk_travelogue_like_travelogue_id FOREIGN KEY (travelogue_id) REFERENCES travelogue (id);
+
+ALTER TABLE travelogue_photo
+    ADD CONSTRAINT fk_travelogue_photo_travelogue_place_id FOREIGN KEY (travelogue_place_id) REFERENCES travelogue_place (id);
+
+ALTER TABLE travelogue_place
+    ADD CONSTRAINT fk_travelogue_place_place_id FOREIGN KEY (place_id) REFERENCES place (id);
+
+ALTER TABLE travelogue_place
+    ADD CONSTRAINT fk_travelogue_place_travelogue_day_id FOREIGN KEY (travelogue_day_id) REFERENCES travelogue_day (id);
+
+ALTER TABLE travelogue_tag
+    ADD CONSTRAINT fk_travelogue_tag_tag_id FOREIGN KEY (tag_id) REFERENCES tag (id);
+
+ALTER TABLE travelogue_tag
+    ADD CONSTRAINT fk_travelogue_tag_travelogue_id FOREIGN KEY (travelogue_id) REFERENCES travelogue (id);
+
+ALTER TABLE travel_place_todo
+    ADD CONSTRAINT fk_travel_place_todo_travel_plan_place_id FOREIGN KEY (travel_plan_place_id) REFERENCES travel_plan_place (id);
+
+ALTER TABLE travel_plan
+    ADD CONSTRAINT fk_travel_plan_author_id FOREIGN KEY (author_id) REFERENCES member (id);
+
+ALTER TABLE travel_plan_day
+    ADD CONSTRAINT fk_travel_plan_day_plan_id FOREIGN KEY (plan_id) REFERENCES travel_plan (id);
+
+ALTER TABLE travel_plan_place
+    ADD CONSTRAINT fk_travel_plan_place_plan_day_id FOREIGN KEY (plan_day_id) REFERENCES travel_plan_day (id);
+
+ALTER TABLE travel_plan_place
+    ADD CONSTRAINT fk_travel_plan_place_place_id FOREIGN KEY (place_id) REFERENCES place (id);
