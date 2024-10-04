e6358d6bfa1a59564739a742bb006239a8ea3e65
libienz <leekh10195@gmail.com>
2024-10-04 12:48:42 +0900

    refactor: 양방향 연관관계를 통해 Dto 매핑 과정에서 엔티티 연관관계를 맺도록 수정

--- backend/src/main/java/kr/touroot/travelogue/dto/request/TravelogueRequest.java
+++ backend/src/main/java/kr/touroot/travelogue/dto/request/TravelogueRequest.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Size;
 import java.util.List;
 import kr.touroot.member.domain.Member;
 import kr.touroot.travelogue.domain.Travelogue;
+import kr.touroot.travelogue.domain.TravelogueDay;
 
 public record TravelogueRequest(
         @Schema(description = "여행기 제목", example = "서울 강남 여행기")
@@ -30,5 +31,19 @@ public record TravelogueRequest(
 
     public Travelogue toTravelogueOf(Member author, String url) {
         return new Travelogue(author, title, url);
+    }
+
+    public Travelogue toTravelogue(Member author) {
+        Travelogue travelogue = new Travelogue(author, title, thumbnail);
+        addTravelogueDays(travelogue);
+        return travelogue;
+    }
+
+    private void addTravelogueDays(Travelogue travelogue) {
+        for (int dayOrder = 0; dayOrder < days.size(); dayOrder++) {
+            TravelogueDayRequest dayRequest = days.get(dayOrder);
+            TravelogueDay travelogueDay = dayRequest.toTravelogueDay(dayOrder, travelogue);
+            travelogue.addDay(travelogueDay);
+        }
     }
 }
