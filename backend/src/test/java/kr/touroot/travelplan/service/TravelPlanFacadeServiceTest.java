9ab1f8e1a3cf4ed3188a481a64a05477d6c8732a
libienz <leekh10195@gmail.com>
2024-10-05 23:11:55 +0900

    test: 여행 계획 생성 FacadeService 테스트 작성

---
+++ backend/src/test/java/kr/touroot/travelplan/service/TravelPlanFacadeServiceTest.java
@@ -0,0 +1,86 @@
+package kr.touroot.travelplan.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import kr.touroot.authentication.infrastructure.PasswordEncryptor;
+import kr.touroot.global.ServiceTest;
+import kr.touroot.global.auth.dto.MemberAuth;
+import kr.touroot.member.domain.Member;
+import kr.touroot.member.service.MemberService;
+import kr.touroot.travelplan.dto.request.PlanDayRequest;
+import kr.touroot.travelplan.dto.request.PlanPlaceRequest;
+import kr.touroot.travelplan.dto.request.PlanPositionRequest;
+import kr.touroot.travelplan.dto.request.PlanRequest;
+import kr.touroot.travelplan.dto.response.PlanCreateResponse;
+import kr.touroot.travelplan.helper.TravelPlanTestHelper;
+import kr.touroot.utils.DatabaseCleaner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+
+@DisplayName("여행 계획 파사드 서비스 테스트")
+@Import({
+        TravelPlanFacadeService.class,
+        TravelPlanService.class,
+        MemberService.class,
+        PasswordEncryptor.class,
+        TravelPlanTestHelper.class
+})
+@ServiceTest
+class TravelPlanFacadeServiceTest {
+
+    private final TravelPlanFacadeService travelPlanFacadeService;
+    private final DatabaseCleaner databaseCleaner;
+    private final TravelPlanTestHelper testHelper;
+
+    private MemberAuth memberAuth;
+    private Member author;
+
+    @Autowired
+    public TravelPlanFacadeServiceTest(
+            TravelPlanFacadeService travelPlanFacadeService,
+            DatabaseCleaner databaseCleaner,
+            TravelPlanTestHelper testHelper
+    ) {
+        this.travelPlanFacadeService = travelPlanFacadeService;
+        this.databaseCleaner = databaseCleaner;
+        this.testHelper = testHelper;
+    }
+
+    @BeforeEach
+    void setUp() {
+        databaseCleaner.executeTruncate();
+
+        author = testHelper.initMemberTestData();
+        memberAuth = new MemberAuth(author.getId());
+    }
+
+    @DisplayName("여행 계획 서비스는 여행 계획 생성 시 생성된 id를 응답한다.")
+    @Test
+    void createTravelPlan() {
+        // given
+        PlanPositionRequest locationRequest = new PlanPositionRequest("37.5175896", "127.0867236");
+        PlanPlaceRequest planPlaceRequest = PlanPlaceRequest.builder()
+                .placeName("잠실한강공원")
+                .todos(Collections.EMPTY_LIST)
+                .position(locationRequest)
+                .build();
+        PlanDayRequest planDayRequest = new PlanDayRequest(List.of(planPlaceRequest));
+        PlanRequest request = PlanRequest.builder()
+                .title("신나는 한강 여행")
+                .startDate(LocalDate.MAX)
+                .days(List.of(planDayRequest))
+                .build();
+
+        // when
+        PlanCreateResponse actual = travelPlanFacadeService.createTravelPlan(request, memberAuth);
+
+        // then
+        assertThat(actual.id()).isEqualTo(1L);
+    }
+}
