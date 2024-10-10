59312972b2d5407337fd94a2f3a455f501563994
river <130737187+0jenn0@users.noreply.github.com>
2024-10-10 13:12:25 +0900

    refactor(FloatingButton): 플로팅 버튼에 focus trap 사용

--- frontend/src/components/common/FloatingButton/FloatingButton.tsx
+++ frontend/src/components/common/FloatingButton/FloatingButton.tsx
@@ -5,11 +5,25 @@ import useModalControl from "@hooks/useModalControl";
 
 import { ROUTE_PATHS_MAP } from "@constants/route";
 
+import removeEmojis from "@utils/removeEmojis";
+
 import { PRIMITIVE_COLORS } from "@styles/tokens";
 
+import FocusTrap from "../FocusTrap";
 import IconButton from "../IconButton/IconButton";
 import Text from "../Text/Text";
 import * as S from "./FloatingButton.styled";
+
+const SUB_BUTTONS = [
+  {
+    text: "✈️ 여행 계획 작성",
+    route: ROUTE_PATHS_MAP.travelPlanRegister,
+  },
+  {
+    text: "📝 여행기 작성",
+    route: ROUTE_PATHS_MAP.travelogueRegister,
+  },
+];
 
 const FloatingButton = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,31 +33,39 @@ const FloatingButton = () => {
     setIsOpen((prev) => !prev);
   };
 
-  const handleClickTravelogueRegister = () => {
-    navigate(ROUTE_PATHS_MAP.travelogueRegister);
-  };
-
-  const handleClickTravelPlanRegister = () => {
-    navigate(ROUTE_PATHS_MAP.travelPlanRegister);
+  const handleClickSubButton = (route: string) => () => {
+    navigate(route);
   };
 
   useModalControl(isOpen, handleToggleButton);
 
   return (
     <S.FloatingButtonContainer>
-      {isOpen && <S.BackdropLayout onClick={handleToggleButton} />}
-      <S.SubButtonContainer $isOpen={isOpen}>
-        <S.SubButton onClick={handleClickTravelPlanRegister}>
-          <Text textType="body" css={S.subButtonTextStyle}>
-            ✈️ 여행 계획 작성
-          </Text>
-        </S.SubButton>
-        <S.SubButton onClick={handleClickTravelogueRegister}>
-          <Text textType="body" css={S.subButtonTextStyle}>
-            📝 여행기 작성
-          </Text>
-        </S.SubButton>
-      </S.SubButtonContainer>
+      <div aria-live="polite">
+        {isOpen
+          ? "여행기 및 여행 계획 작성 모달이 열렸습니다. 닫으려면 esc버튼을 눌러주세요."
+          : "여행기 및 여행 계획 작성 모달이 닫혔습니다."}
+      </div>
+      {isOpen && (
+        <>
+          <S.BackdropLayout onClick={handleToggleButton} />
+          <FocusTrap>
+            <S.SubButtonContainer $isOpen={isOpen}>
+              {SUB_BUTTONS.map(({ text, route }) => (
+                <S.SubButton
+                  key={route}
+                  onClick={handleClickSubButton(route)}
+                  aria-label={removeEmojis(text)}
+                >
+                  <Text textType="body" css={S.subButtonTextStyle}>
+                    {text}
+                  </Text>
+                </S.SubButton>
+              ))}
+            </S.SubButtonContainer>
+          </FocusTrap>
+        </>
+      )}
 
       <S.MainButtonWrapper onClick={handleToggleButton} $isOpen={isOpen}>
         <IconButton
