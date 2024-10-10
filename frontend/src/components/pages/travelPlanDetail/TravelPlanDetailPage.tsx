aec9e5a5183dcbf9ab2912287c0b1986de399dbd
simhorim <hori5020@gmail.com>
2024-10-10 11:18:40 +0900

    refactor(Detail): 전환이라는 용어 낯설다는 피드백 반영

--- frontend/src/components/pages/travelPlanDetail/TravelPlanDetailPage.tsx
+++ frontend/src/components/pages/travelPlanDetail/TravelPlanDetailPage.tsx
@@ -152,7 +152,7 @@ const TravelPlanDetailPage = () => {
 
       <TransformFooter
         onTransform={handleTransform}
-        buttonLabel="여행기로 전환"
+        buttonLabel="여행기 남기기"
         guideMessage="여행은 즐겁게 다녀오셨나요?"
       />
 
