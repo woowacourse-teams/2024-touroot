932126355aa0d255b154b9a49791b1d407f4719c
simhorim <hori5020@gmail.com>
2024-10-16 10:29:02 +0900

    feat(MyPage): 프로필 이미지 수정 기능 구현

--- frontend/src/components/pages/my/MyPage.tsx
+++ frontend/src/components/pages/my/MyPage.tsx
@@ -1,9 +1,12 @@
-import { FormEvent, MouseEvent, useEffect, useState } from "react";
-
-import usePatchNickname from "@queries/usePatchNickname";
-import { useUserProfile } from "@queries/useUserProfile";
-
-import { AvatarCircle, CharacterCount, Input, Tab, Text } from "@components/common";
+import {
+  AvatarCircle,
+  CharacterCount,
+  IconButton,
+  Input,
+  Spinner,
+  Tab,
+  Text,
+} from "@components/common";
 import MyPageSkeleton from "@components/pages/my/MyPageSkeleton/MyPageSkeleton";
 
 import { ERROR_MESSAGE_MAP } from "@constants/errorMessage";
@@ -13,6 +16,8 @@ import { STORAGE_KEYS_MAP } from "@constants/storage";
 import * as S from "./MyPage.styled";
 import MyTravelPlans from "./MyTravelPlans/MyTravelPlans";
 import MyTravelogues from "./MyTravelogues/MyTravelogues";
+import ProfileImageEditModalBottomSheet from "./ProfileImageEditModalBottomSheet/ProfileImageEditModalBottomSheet";
+import useMyPage from "./hooks/useMyPage";
 
 const TAB_CONTENT = [
   { label: "✈️ 내 여행 계획", component: MyTravelPlans },
@@ -20,102 +25,105 @@ const TAB_CONTENT = [
   { label: "❤️ 좋아요", component: MyTravelogues }, // 추후 컴포넌트 바꿀 예정
 ] as const;
 
+const IGNORED_ERROR_MESSAGES = [ERROR_MESSAGE_MAP.api.login, "Network Error"];
+
 const MyPage = () => {
-  const { data, status, error } = useUserProfile();
+  const { states, handlers, userProfile, profileImageFileInputRef } = useMyPage();
 
-  const [isModifying, setIsModifying] = useState(false);
-  const [nickname, setNickname] = useState(data?.nickname ?? "");
-
-  const onError = (error: Error) => {
-    alert(error.message);
-    setNickname(data?.nickname ?? "");
+  const showErrorAlert = (error: Error | null) => {
+    if (error && !IGNORED_ERROR_MESSAGES.includes(error.message)) alert(error.message);
   };
 
-  const { mutate: mutateModifyNickname } = usePatchNickname(onError);
+  if (userProfile.status === "pending") return <MyPageSkeleton />;
 
-  useEffect(() => {
-    if (data?.nickname) {
-      setNickname(data.nickname);
-    }
-  }, [data?.nickname]);
-
-  const handleStartNicknameEdit = () => {
-    setIsModifying(true);
-  };
-
-  const handleSubmitNicknameChange = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const trimmedNickname = nickname.trim();
-    const newNickname = trimmedNickname || data?.nickname || "";
-
-    setNickname(newNickname);
-    mutateModifyNickname(newNickname);
-
-    setIsModifying(false);
-  };
-
-  const handleButtonClick = (e: MouseEvent<HTMLButtonElement>) => {
-    if (!isModifying) {
-      e.preventDefault();
-      handleStartNicknameEdit();
-    }
-  };
-
-  if (status === "pending" || status === "error") {
-    if (
-      status === "error" &&
-      error.message !== ERROR_MESSAGE_MAP.api.login &&
-      error.message !== "Network Error"
-    ) {
-      alert(error.message);
-    }
+  if (userProfile.status === "error") {
+    showErrorAlert(userProfile.error);
 
     return <MyPageSkeleton />;
   }
 
   return (
     <S.Layout>
-      <S.FormWrapper onSubmit={handleSubmitNicknameChange}>
-        <S.ButtonWrapper>
-          <S.Button type={isModifying ? "submit" : "button"} onClick={handleButtonClick}>
-            {isModifying ? "확인" : "프로필 수정"}
-          </S.Button>
-        </S.ButtonWrapper>
+      {states.isModifying ? (
+        <S.ProfileContainer>
+          <S.EditButtonContainer>
+            <S.ProfileEditButtonContainer>
+              <S.EditButton type="button" onClick={handlers.handleClickProfileEditCancelButton}>
+                취소
+              </S.EditButton>
+              <S.EditButton type="button" onClick={handlers.handleClickProfileEditConfirmButton}>
+                확인
+              </S.EditButton>
+            </S.ProfileEditButtonContainer>
+          </S.EditButtonContainer>
 
-        <AvatarCircle $size="large" profileImageUrl={data?.profileImageUrl} />
+          <S.ProfileImageContainer>
+            <S.ProfileImageWrapper $isProfileImageLoading={states.isProfileImageLoading}>
+              <AvatarCircle
+                size="large"
+                profileImageUrl={states.profileImageUrl}
+                onLoad={handlers.handleLoadProfileImage}
+              />
+            </S.ProfileImageWrapper>
 
-        <S.NicknameWrapper>
-          {!isModifying ? (
-            <Text textType="body" css={S.NicknameStyle}>
-              {nickname}
+            {states.isProfileImageLoading ? (
+              <S.ProfileImageLoadingWrapper>
+                <Spinner variants="circle" size={40} />
+              </S.ProfileImageLoadingWrapper>
+            ) : (
+              <>
+                <S.ProfileImageHiddenInput
+                  ref={profileImageFileInputRef}
+                  type="file"
+                  accept="image/*"
+                  onChange={handlers.handleChangeProfileImage}
+                  aria-label="썸네일 이미지 선택"
+                  title="이미지 파일을 선택하세요"
+                />
+                <IconButton
+                  iconType="camera-icon"
+                  onClick={handlers.handleClickEditModalOpenButton}
+                  css={S.profileImageEditButtonStyle}
+                />
+              </>
+            )}
+          </S.ProfileImageContainer>
+
+          <S.NickNameEditContainer>
+            <Input
+              placeholder={userProfile.data?.nickname}
+              value={states.nickname}
+              autoFocus
+              maxLength={FORM_VALIDATIONS_MAP.title.maxLength}
+              spellCheck={false}
+              css={S.inputStyle}
+              onChange={handlers.handleChangeNickname}
+            />
+            <CharacterCount
+              count={states.nickname?.length}
+              maxCount={FORM_VALIDATIONS_MAP.title.maxLength}
+            />
+          </S.NickNameEditContainer>
+        </S.ProfileContainer>
+      ) : (
+        <S.ProfileContainer>
+          <S.EditButtonContainer>
+            <S.EditButton type="button" onClick={handlers.handleClickProfileEditButton}>
+              프로필 수정
+            </S.EditButton>
+          </S.EditButtonContainer>
+
+          <S.ProfileImageContainer>
+            <AvatarCircle size="large" profileImageUrl={states.profileImageUrl} />
+          </S.ProfileImageContainer>
+
+          <S.NicknameWrapper>
+            <Text textType="bodyBold" css={S.nicknameStyle}>
+              {states.nickname}
             </Text>
-          ) : (
-            <S.InputContainer>
-              <Input
-                placeholder={data?.nickname}
-                value={nickname}
-                autoFocus
-                maxLength={FORM_VALIDATIONS_MAP.title.maxLength}
-                spellCheck={false}
-                css={S.inputStyle}
-                onChange={(e) =>
-                  setNickname(
-                    e.target.value.slice(
-                      FORM_VALIDATIONS_MAP.title.minLength,
-                      FORM_VALIDATIONS_MAP.title.maxLength,
-                    ),
-                  )
-                }
-              />
-              <CharacterCount
-                count={nickname?.length}
-                maxCount={FORM_VALIDATIONS_MAP.title.maxLength}
-              />
-            </S.InputContainer>
-          )}
-        </S.NicknameWrapper>
-      </S.FormWrapper>
+          </S.NicknameWrapper>
+        </S.ProfileContainer>
+      )}
 
       <Tab
         storageKey={STORAGE_KEYS_MAP.myPageSelectedTabIndex}
@@ -123,10 +131,26 @@ const MyPage = () => {
         tabContent={(selectedIndex) => {
           const SelectedComponent = TAB_CONTENT[selectedIndex].component;
 
-          return data ? <SelectedComponent userData={data} /> : <></>;
+          return userProfile.data ? <SelectedComponent userData={userProfile.data} /> : <></>;
         }}
-        css={S.ListStyle}
+        css={S.listStyle}
       />
+
+      <ProfileImageEditModalBottomSheet
+        isOpen={states.isModalOpen}
+        onClose={handlers.handleClickEditModalCloseButton}
+      >
+        <S.Button onClick={handlers.handleClickProfileImageEditButton}>
+          <Text textType="detail">앨범에서 선택</Text>
+        </S.Button>
+        {states.profileImageUrl && (
+          <S.Button onClick={handlers.handleClickProfileImageDeleteButton}>
+            <Text textType="detailBold" css={S.deleteTextColor}>
+              프로필 사진 삭제
+            </Text>
+          </S.Button>
+        )}
+      </ProfileImageEditModalBottomSheet>
     </S.Layout>
   );
 };
