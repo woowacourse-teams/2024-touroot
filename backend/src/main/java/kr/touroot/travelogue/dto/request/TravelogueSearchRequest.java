068bb1066edbacae459d73981cc65e8f0ba84c4c
slimsha2dy <chy2581@naver.com>
2024-10-20 02:39:24 +0900

    feat: 국가 기반 검색 기능 구현

--- backend/src/main/java/kr/touroot/travelogue/dto/request/TravelogueSearchRequest.java
+++ backend/src/main/java/kr/touroot/travelogue/dto/request/TravelogueSearchRequest.java
@@ -9,7 +9,7 @@ public record TravelogueSearchRequest(
         @NotBlank(message = "검색어는 2글자 이상이어야 합니다.")
         @Size(min = 2, message = "검색어는 2글자 이상이어야 합니다.")
         String keyword,
-        @Schema(description = "검색 키워드 종류 (TITLE, AUTHOR)", example = "TITLE")
+        @Schema(description = "검색 키워드 종류 (TITLE, AUTHOR, COUNTRY)", example = "TITLE")
         @NotBlank(message = "검색 키워드 종류는 필수입니다.")
         String searchType
 ) {
--- backend/src/main/java/kr/touroot/travelogue/repository/query/TravelogueQueryRepository.java
+++ backend/src/main/java/kr/touroot/travelogue/repository/query/TravelogueQueryRepository.java
@@ -2,6 +2,7 @@ package kr.touroot.travelogue.repository.query;
 
 import kr.touroot.travelogue.domain.Travelogue;
 import kr.touroot.travelogue.domain.TravelogueFilterCondition;
+import kr.touroot.travelogue.domain.search.CountryCode;
 import kr.touroot.travelogue.domain.search.SearchCondition;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 public interface TravelogueQueryRepository {
 
     Page<Travelogue> findByKeywordAndSearchType(SearchCondition condition, Pageable pageable);
+
+    Page<Travelogue> findByKeywordAndCountryCode(CountryCode countryCode, Pageable pageable);
 
     Page<Travelogue> findAllByFilter(TravelogueFilterCondition filter, Pageable pageable);
 }
