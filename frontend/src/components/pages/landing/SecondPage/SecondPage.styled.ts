4b6114b8fdd14bc1535785cf999f40172f6f72e0
river <130737187+0jenn0@users.noreply.github.com>
2024-10-19 16:26:41 +0900

    feat(SecondPage): 랜딩 페이지의 두번째 페이지 구현

---
+++ frontend/src/components/pages/landing/SecondPage/SecondPage.styled.ts
@@ -0,0 +1,159 @@
+import { css, keyframes } from "@emotion/react";
+
+import theme from "@styles/theme";
+import { PRIMITIVE_COLORS } from "@styles/tokens";
+
+import { fadeIn } from "../Animation.styled";
+
+export const layout = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  width: 100%;
+  max-width: 48rem;
+  padding: ${theme.spacing.l};
+  gap: ${theme.spacing.xxl};
+
+  background-color: ${PRIMITIVE_COLORS.white};
+`;
+
+export const contentContainer = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  gap: ${theme.spacing.xl};
+`;
+
+export const boxContainer = css`
+  display: flex;
+  position: relative;
+`;
+
+export const emojiText = css`
+  position: absolute;
+  right: -3rem;
+  bottom: -2rem;
+
+  font-size: 4rem;
+`;
+
+export const relativeBox = css`
+  position: relative;
+  width: 80%;
+`;
+
+export const absoluteEmojiText = css`
+  position: absolute;
+  bottom: 0.4rem;
+  left: 0;
+
+  font-size: 4rem;
+`;
+
+export const section = css`
+  position: relative;
+`;
+
+export const textContainer = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  gap: ${theme.spacing.s};
+`;
+
+export const mainImage = css`
+  width: 100%;
+`;
+
+export const bottomBox = css`
+  justify-content: center;
+  gap: ${theme.spacing.m};
+`;
+
+export const bottomTextContainer = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const centeredTextContainer = css`
+  display: flex;
+  justify-content: center;
+`;
+
+export const fontWeightMedium = css`
+  font-weight: 500;
+`;
+
+export const emojiStyle = css`
+  position: absolute;
+  top: 5%;
+  left: 30%;
+  transform: translateY(50%);
+  transform: translateX(-50%);
+  z-index: 10;
+`;
+
+const float = keyframes`
+  0% { transform: translateY(0px); }
+  50% { transform: translateY(-1rem); }
+  100% { transform: translateY(0px); }
+`;
+
+export const buttonStyle = css`
+  position: absolute;
+  top: -5.8rem;
+  z-index: ${theme.zIndex.toast};
+  padding: ${theme.spacing.m};
+  opacity: 0.5;
+
+  background-color: transparent;
+
+  animation: ${float} 1s ease-in-out infinite;
+`;
+
+export const primaryText = css`
+  color: ${theme.colors.primary};
+`;
+
+export const createAnimatedSectionStyle = ({
+  isVisible,
+  delay,
+}: {
+  isVisible: boolean;
+  delay?: number;
+}) => css`
+  opacity: 0;
+  transform: translateY(20px);
+  transition:
+    opacity 0.5s ease-out,
+    transform 0.5s ease-out;
+
+  ${isVisible &&
+  css`
+    animation: ${fadeIn} 0.5s ease-out forwards;
+  `}
+
+  ${delay &&
+  css`
+    animation-delay: ${delay}ms;
+  `};
+`;
+
+export const widthEightyPercent = css`
+  width: 80%;
+`;
+
+export const widthFull = css`
+  width: 100%;
+`;
+
+export const alignEnd = css`
+  display: flex;
+  justify-content: flex-end;
+`;
---
+++ frontend/src/components/pages/landing/SecondPage/SecondPage.tsx
@@ -0,0 +1,189 @@
+import { Icon, Text } from "@components/common";
+
+import { PRIMITIVE_COLORS } from "@styles/tokens";
+
+import { MainPageImage } from "@assets/webp";
+
+import { createAnimation } from "../Animation.styled";
+import Box from "../components/Box";
+import useAnimationObserver from "../hook/useAnimationObserver";
+import * as S from "./SecondPage.styled";
+
+const SecondPage = () => {
+  const { registerElement, getAnimationState } = useAnimationObserver();
+
+  return (
+    <div css={S.layout}>
+      <Icon iconType="down-arrow" size="60" color={PRIMITIVE_COLORS.white} css={S.buttonStyle} />
+      <div css={S.contentContainer}>
+        <Box
+          ref={registerElement("Box1")}
+          css={[
+            S.widthEightyPercent,
+            createAnimation({
+              shouldAnimate: getAnimationState("Box1").shouldAnimate,
+              animation: "fadeIn",
+            }),
+          ]}
+        >
+          <div css={S.boxContainer}>
+            <Text css={S.emojiText}>🤔</Text>
+            <Text isInline>다른 사람의 여행을</Text>
+            <Text isInline textType="bodyBold">
+              따라가고 싶은데,
+            </Text>
+          </div>
+          <div>
+            <Text isInline>정리하기는 </Text>
+            <Text textType="bodyBold" isInline>
+              귀찮은 적{" "}
+            </Text>
+            <Text isInline>있지 않으셨나요?</Text>
+          </div>
+        </Box>
+
+        <Box
+          ref={registerElement("Box2")}
+          css={[
+            S.relativeBox,
+            createAnimation({
+              shouldAnimate: getAnimationState("Box2").shouldAnimate,
+              animation: "fadeIn",
+              delay: 600,
+            }),
+          ]}
+        >
+          <Text css={S.absoluteEmojiText}>🤯</Text>
+          <div css={S.alignEnd}>
+            <Text isInline>방대한 양의 여행을</Text>
+            <Text textType="bodyBold" isInline>
+              어떻게 기록할지
+            </Text>
+          </div>
+          <div css={S.alignEnd}>
+            <Text textType="bodyBold" isInline>
+              막막한 적
+            </Text>
+            <Text isInline>있지 않으셨나요?</Text>
+          </div>
+        </Box>
+      </div>
+
+      <section css={S.section}>
+        <div
+          ref={registerElement("Text1")}
+          css={[
+            S.textContainer,
+            createAnimation({
+              shouldAnimate: getAnimationState("Text1").shouldAnimate,
+              animation: "fadeIn",
+              delay: 900,
+            }),
+          ]}
+        >
+          <div>
+            <Text textType="subTitle" isInline css={S.primaryText}>
+              단 한 번의 클릭
+            </Text>
+            <Text textType="subTitle" isInline css={S.fontWeightMedium}>
+              으로
+            </Text>
+          </div>
+          <div>
+            <Text textType="subTitle" isInline>
+              다른 사람의 여행
+            </Text>
+            <Text textType="subTitle" isInline css={S.fontWeightMedium}>
+              이
+            </Text>
+          </div>
+          <div>
+            <Text textType="subTitle" isInline>
+              나의 여행 계획
+            </Text>
+            <Text textType="subTitle" isInline css={S.fontWeightMedium}>
+              으로 바뀌고
+            </Text>
+          </div>
+        </div>
+
+        <img
+          src={MainPageImage}
+          alt="투룻 메인 페이지 이미지"
+          ref={registerElement("Image1")}
+          css={[
+            S.mainImage,
+            createAnimation({
+              shouldAnimate: getAnimationState("Image1").shouldAnimate,
+              animation: "fadeIn",
+            }),
+          ]}
+        />
+
+        <div
+          ref={registerElement("Text2")}
+          css={[
+            S.textContainer,
+            createAnimation({
+              shouldAnimate: getAnimationState("Text2").shouldAnimate,
+              animation: "fadeIn",
+            }),
+          ]}
+        >
+          <div>
+            <Text textType="subTitle" isInline>
+              미리 준비된 템플릿
+            </Text>
+            <Text textType="subTitle" isInline css={S.fontWeightMedium}>
+              을 따라
+            </Text>
+          </div>
+          <div>
+            <Text textType="subTitle" isInline>
+              간편하게 여행
+            </Text>
+            <Text textType="subTitle" isInline css={S.fontWeightMedium}>
+              을{" "}
+            </Text>
+            <Text textType="subTitle" isInline css={S.primaryText}>
+              기록할 수 있는 곳
+            </Text>
+          </div>
+        </div>
+      </section>
+
+      <Box
+        ref={registerElement("Box4")}
+        css={[
+          S.bottomBox,
+          createAnimation({
+            shouldAnimate: getAnimationState("Box4").shouldAnimate,
+            animation: "fadeIn",
+            delay: 300,
+          }),
+        ]}
+      >
+        <div css={S.bottomTextContainer}>
+          <Text>귀찮은 기억은 내려놓고</Text>
+          <div>
+            <Text textType="bodyBold" isInline>
+              즐거운 기억
+            </Text>
+            <Text isInline>만 가져갈 수 있도록</Text>
+          </div>
+        </div>
+
+        <div css={S.centeredTextContainer}>
+          <Text textType="subTitle" isInline>
+            투룻
+          </Text>
+          <Text textType="subTitle" isInline>
+            이 도와줄게요!
+          </Text>
+        </div>
+      </Box>
+    </div>
+  );
+};
+
+export default SecondPage;
