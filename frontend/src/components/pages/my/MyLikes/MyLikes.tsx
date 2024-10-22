4a3494956893d53ccfafcf1e1890b4a331b5f21a
simorimi <141118352+simorimi@users.noreply.github.com>
2024-10-22 18:34:44 +0900

    [Feature] - 마이페이지 좋아요 탭 및 검색 창 국가 탭 기능 구현 (#556)
    * refactor(MyPage): 프로필 이미지 수정 모달 text 변경
    
    * feat(TravelogueList): 여행기 검색 나라 탭 추가
    
    * refactor(Tab): 스타일링 수정
    
    * refactor(MyTravelogue): 불필요한 스타일 제거 및 일관성을 위하여 게시일 텍스트 제거
    
    * feat(useInfiniteMyLikes): 훅 기능 구현
    
    * feat(MyLikes): 좋아요 탭 UI 구현
    
    * refactor: 좋아요 버튼 클릭시 마이페이지 좋아요 무효화 하도록 수정
    
    * refactor(MyLikes): 마이페이지텝 컴포넌트 좋아요 탭에서도 재사용하도록 수정

--- frontend/src/components/pages/my/MyLikes/MyLikes.tsx
+++ frontend/src/components/pages/my/MyLikes/MyLikes.tsx
@@ -5,7 +5,7 @@ import { css } from "@emotion/react";
 
 import type { UserResponse } from "@type/domain/user";
 
-import useInfiniteMyTravelogues from "@queries/useInfiniteMyTravelogues";
+import useInfiniteMyLikes from "@queries/useInfiniteMyLikes";
 
 import { AvatarCircle, Text } from "@components/common";
 import MyPageTabContentSkeleton from "@components/pages/my/MyPageTabContentSkeleton/MyPageTabContentSkeleton";
@@ -17,26 +17,25 @@ import { ROUTE_PATHS_MAP } from "@constants/route";
 
 import theme from "@styles/theme";
 
-import TabContent from "../MyPageTabContent/MyPageTabContent";
-import * as S from "./MyTravelogues.styled";
+import MyPageTabContent from "../MyPageTabContent/MyPageTabContent";
+import * as S from "./MyLikes.styled";
 
-interface MyTraveloguesProps {
+interface MyLikesProps {
   userData: UserResponse;
 }
 
-const MyTravelogues = ({ userData }: MyTraveloguesProps) => {
-  const { myTravelogues, fetchNextPage, status, isPaused, error } =
-    useInfiniteMyTravelogues(userData);
+const MyLikes = ({ userData }: MyLikesProps) => {
+  const { myLikes, fetchNextPage, status, isPaused, error } = useInfiniteMyLikes(userData);
   const { lastElementRef } = useIntersectionObserver(fetchNextPage);
 
   const navigate = useNavigate();
 
-  const handleClickAddButton = () => {
-    navigate(ROUTE_PATHS_MAP.travelogueRegister);
-  };
-
   const handleClickTravelogue = (id: string) => {
     navigate(ROUTE_PATHS_MAP.travelogue(Number(id)));
+  };
+
+  const handleClickIconButton = () => {
+    navigate(ROUTE_PATHS_MAP.root);
   };
 
   useEffect(() => {
@@ -49,13 +48,15 @@ const MyTravelogues = ({ userData }: MyTraveloguesProps) => {
 
   return (
     <>
-      <TabContent
-        iconButtonLabel="새 여행기 추가하기"
-        onClickIconButton={handleClickAddButton}
-        contentDetail={myTravelogues}
-        renderItem={({ id, thumbnailUrl, title, createdAt }) => (
+      <MyPageTabContent
+        iconButtonType="search-icon"
+        iconButtonLabel="다른 여행기 구경하기"
+        onClickIconButton={handleClickIconButton}
+        contentDetail={myLikes}
+        renderItem={({ id, title, createdAt, thumbnailUrl, authorName }) => (
           <S.Layout onClick={() => handleClickTravelogue(id)}>
             <AvatarCircle size="medium" profileImageUrl={thumbnailUrl} />
+
             <S.Container>
               <Text
                 textType="body"
@@ -65,18 +66,29 @@ const MyTravelogues = ({ userData }: MyTraveloguesProps) => {
               >
                 {title}
               </Text>
-              <Text
-                textType="detail"
-                css={css`
-                  color: ${theme.colors.text.secondary};
-                `}
-              >
-                {createdAt}
-              </Text>
+              <S.DetailContainer>
+                <Text
+                  textType="detail"
+                  css={css`
+                    color: ${theme.colors.text.secondary};
+                  `}
+                >
+                  {authorName}
+                </Text>
+                <Text
+                  textType="detail"
+                  css={css`
+                    color: ${theme.colors.text.secondary};
+                  `}
+                >
+                  {createdAt}
+                </Text>
+              </S.DetailContainer>
             </S.Container>
           </S.Layout>
         )}
       />
+
       <div
         ref={lastElementRef}
         css={css`
@@ -87,4 +99,4 @@ const MyTravelogues = ({ userData }: MyTraveloguesProps) => {
   );
 };
 
-export default MyTravelogues;
+export default MyLikes;
--- frontend/src/components/pages/my/constants.ts
+++ frontend/src/components/pages/my/constants.ts
@@ -1,11 +1,13 @@
 import { ERROR_MESSAGE_MAP } from "@constants/errorMessage";
 
+import MyLikes from "./MyLikes/MyLikes";
 import MyTravelPlans from "./MyTravelPlans/MyTravelPlans";
 import MyTravelogues from "./MyTravelogues/MyTravelogues";
 
 export const TAB_CONTENT = [
   { label: "✈️ 내 여행 계획", component: MyTravelPlans },
   { label: "📝 내 여행기", component: MyTravelogues },
+  { label: "❤️ 좋아요", component: MyLikes },
 ] as const;
 
 export const IGNORED_ERROR_MESSAGES = [ERROR_MESSAGE_MAP.api.login, "Network Error"];
