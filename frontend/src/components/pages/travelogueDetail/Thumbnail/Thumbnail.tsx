2c73b437d629624c2ab74c097c338ef9ee852be1
simhorim <hori5020@gmail.com>
2024-10-22 23:29:28 +0900

    refactor(Thumbnail): alt 추가

--- frontend/src/components/pages/travelogueDetail/Thumbnail/Thumbnail.tsx
+++ frontend/src/components/pages/travelogueDetail/Thumbnail/Thumbnail.tsx
@@ -6,14 +6,19 @@ import * as S from "./Thumbnail.styled";
 
 interface ThumbnailProps extends React.HTMLAttributes<HTMLElement> {
   imageUrl?: string;
+  alt?: string;
 }
 
-const Thumbnail = ({ imageUrl }: ThumbnailProps) => {
+const Thumbnail = ({ imageUrl, alt }: ThumbnailProps) => {
   const { imageError, handleImageError } = useImageError({ imageUrl });
 
   return (
     <S.Wrapper>
-      {imageError ? <FallbackImage /> : <S.Image onError={handleImageError} src={imageUrl} />}
+      {imageError ? (
+        <FallbackImage />
+      ) : (
+        <S.Image onError={handleImageError} src={imageUrl} alt={alt} />
+      )}
     </S.Wrapper>
   );
 };
