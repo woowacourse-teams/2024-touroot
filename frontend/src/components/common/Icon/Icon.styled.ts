63fb8acb3355bfa2ac2b59dece1abe596dfc6e88
river <130737187+0jenn0@users.noreply.github.com>
2024-10-23 19:28:33 +0900

    style(Icon): 가운데 정렬 추가

--- frontend/src/components/common/Icon/Icon.styled.ts
+++ frontend/src/components/common/Icon/Icon.styled.ts
@@ -2,6 +2,8 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div<{ size: string }>`
   display: flex;
+  justify-content: center;
   align-items: center;
+  width: ${({ size }) => size}px;
   height: ${({ size }) => size}px;
 `;
