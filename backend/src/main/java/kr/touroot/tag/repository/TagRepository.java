d36abe66290e1a03b735e05712b1fe07f0e4812e
nhlee98 <e5slnh9876@gmail.com>
2024-10-24 13:19:31 +0900

    feat: 태그 생성 기능 삭제

--- backend/src/main/java/kr/touroot/tag/repository/TagRepository.java
+++ backend/src/main/java/kr/touroot/tag/repository/TagRepository.java
@@ -4,6 +4,4 @@ import kr.touroot.tag.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
-
-    boolean existsByTag(String tag);
 }
--- backend/src/main/java/kr/touroot/tag/service/TagService.java
+++ backend/src/main/java/kr/touroot/tag/service/TagService.java
@@ -1,9 +1,6 @@
 package kr.touroot.tag.service;
 
 import java.util.List;
-import kr.touroot.global.exception.BadRequestException;
-import kr.touroot.tag.domain.Tag;
-import kr.touroot.tag.dto.TagCreateRequest;
 import kr.touroot.tag.dto.TagResponse;
 import kr.touroot.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,20 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class TagService {
 
     private final TagRepository tagRepository;
-
-    @Transactional
-    public TagResponse createTag(TagCreateRequest tagCreateRequest) {
-        validateDuplicated(tagCreateRequest);
-        Tag savedTag = tagRepository.save(tagCreateRequest.toTag());
-
-        return TagResponse.from(savedTag);
-    }
-
-    private void validateDuplicated(TagCreateRequest tagCreateRequest) {
-        if (tagRepository.existsByTag(tagCreateRequest.tag())) {
-            throw new BadRequestException("이미 존재하는 태그입니다.");
-        }
-    }
 
     @Cacheable(cacheNames = "tag")
     @Transactional(readOnly = true)
