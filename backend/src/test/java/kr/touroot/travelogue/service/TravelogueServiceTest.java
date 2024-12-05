5b496404d26166ea95ae98c13f8fe9c327fe9372
libienz <leekh10195@gmail.com>
2024-12-05 19:34:13 +0900

    test: 사용하지 않는 테스트 상수 제거 개선

--- backend/src/test/java/kr/touroot/travelogue/service/TravelogueServiceTest.java
+++ backend/src/test/java/kr/touroot/travelogue/service/TravelogueServiceTest.java
@@ -37,8 +37,6 @@ import org.springframework.data.domain.Sort;
 @ServiceTest
 class TravelogueServiceTest {
 
-    public static final int BASIC_PAGE_SIZE = 5;
-
     private final TravelogueService travelogueService;
     private final DatabaseCleaner databaseCleaner;
     private final TravelogueTestHelper testHelper;
