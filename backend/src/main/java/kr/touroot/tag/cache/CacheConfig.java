b1fe1af088b8040700c34c59d0c7ae3a86bc3ff0
eunjungL <dldmswjd322@gmail.com>
2024-12-31 14:44:27 +0900

    feat: CacheManager Redis로 변경

--- backend/src/main/java/kr/touroot/tag/cache/CacheConfig.java
+++ backend/src/main/java/kr/touroot/tag/cache/CacheConfig.java
@@ -1,25 +1,52 @@
 package kr.touroot.tag.cache;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.time.Duration;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class CacheConfig {
 
     @Bean
-    public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("tag");
-        cacheManager.setCaffeine(caffeineConfig());
-
-        return cacheManager;
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(getRedisCacheConfiguration())
+                .build();
     }
 
-    private Caffeine<Object, Object> caffeineConfig() {
-        return Caffeine.newBuilder()
-                .expireAfterWrite(Duration.ofDays(1));
+    private RedisCacheConfiguration getRedisCacheConfiguration() {
+        SerializationPair<String> keySerializationPair = SerializationPair.fromSerializer(new StringRedisSerializer());
+        SerializationPair<Object> valueSerializationPair = SerializationPair.fromSerializer(
+                new GenericJackson2JsonRedisSerializer(getObjectMapperForRedisCacheManager())
+        );
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(keySerializationPair)
+                .serializeValuesWith(valueSerializationPair)
+                .entryTtl(Duration.ofHours(1L))
+                .disableCachingNullValues();
+    }
+
+    private ObjectMapper getObjectMapperForRedisCacheManager() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(
+                BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build(),
+                DefaultTyping.EVERYTHING
+        );
+
+        return objectMapper;
     }
 }
