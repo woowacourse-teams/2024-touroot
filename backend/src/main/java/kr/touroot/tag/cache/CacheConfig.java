fdd0cd02765e71d9971692d25344414994115012
eunjungL <dldmswjd322@gmail.com>
2024-12-31 15:01:51 +0900

    refactor: EnableCaching 추가

--- backend/src/main/java/kr/touroot/tag/cache/CacheConfig.java
+++ backend/src/main/java/kr/touroot/tag/cache/CacheConfig.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.time.Duration;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -15,6 +16,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@EnableCaching
 @Configuration
 public class CacheConfig {
 
