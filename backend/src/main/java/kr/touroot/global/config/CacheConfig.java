4006813b2a88d45b0faf0b07f63b0b83160fcfcd
eunjungL <62099953+eunjungL@users.noreply.github.com>
2024-12-31 20:04:36 +0900

    [Feature] - Redis cache 도입 (#629)
    * feat: spring-data-redis 의존성 추가
    
    * feat: CacheManager Redis로 변경
    
    * refactor: EnableCaching 추가
    
    * refactor: CacheConfig package global.config로 이동
    
    * feat: redis 관련 설정 yml 추가
    
    * style: 줄바꿈 컨벤션 준수
    
    * feat: Testcontainers 레디스 모듈 의존성 추가
    
    * feat: Testcontainers 기반 통합테스트 클래스에 레디스 컨테이너 추가
    
    ---------
    
    Co-authored-by: libienz <leekh10195@gmail.com>

---
+++ backend/src/main/java/kr/touroot/global/config/CacheConfig.java
@@ -0,0 +1,54 @@
+package kr.touroot.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.Duration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(getRedisCacheConfiguration())
+                .build();
+    }
+
+    private RedisCacheConfiguration getRedisCacheConfiguration() {
+        SerializationPair<String> keySerializationPair = SerializationPair.fromSerializer(new StringRedisSerializer());
+        SerializationPair<Object> valueSerializationPair = SerializationPair.fromSerializer(
+                new GenericJackson2JsonRedisSerializer(getObjectMapperForRedisCacheManager())
+        );
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(keySerializationPair)
+                .serializeValuesWith(valueSerializationPair)
+                .entryTtl(Duration.ofHours(1L))
+                .disableCachingNullValues();
+    }
+
+    private ObjectMapper getObjectMapperForRedisCacheManager() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.activateDefaultTyping(
+                BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build(),
+                DefaultTyping.EVERYTHING
+        );
+
+        return objectMapper;
+    }
+}
