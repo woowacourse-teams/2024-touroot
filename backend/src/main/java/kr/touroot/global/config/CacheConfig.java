37b982c4674642ee98185b3ca30f313a8c52fc0e
libienz <leekh10195@gmail.com>
2025-01-01 23:47:50 +0900

    refactor: CacheConfig 불필요한 애너테이션 제거 개선

--- backend/src/main/java/kr/touroot/global/config/CacheConfig.java
+++ backend/src/main/java/kr/touroot/global/config/CacheConfig.java
@@ -9,7 +9,6 @@ import java.time.Duration;
 import kr.touroot.global.util.PageDeserializer;
 import kr.touroot.global.util.SortDeserializer;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageImpl;
@@ -20,10 +19,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-@EnableCaching
-@EnableSpringDataWebSupport
 @Configuration
 public class CacheConfig {
 
