4bd3d72d3c1974f0de213cb23b4a9def230e70fe
libienz <leekh10195@gmail.com>
2025-02-06 15:42:41 +0900

    feat: ScheduleConfig에서 스케줄링 Enable

---
+++ backend/src/main/java/kr/touroot/global/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package kr.touroot.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
