77b230452e86908731a626bde79137f0c93b4b53
libienz <leekh10195@gmail.com>
2025-02-06 15:34:51 +0900

    fix: Repository 테스트에서 DataJpaTest로 인해 실 서비스와 다른 트랜잭션이 생성되지 않도록 수정

--- backend/src/test/java/kr/touroot/global/AbstractRepositoryIntegrationTest.java
+++ backend/src/test/java/kr/touroot/global/AbstractRepositoryIntegrationTest.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import({TestQueryDslConfig.class, DatabaseCleaner.class})
 public abstract class AbstractRepositoryIntegrationTest extends AbstractIntegrationTest {
