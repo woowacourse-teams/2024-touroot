f8b46b96f950c72ef4d1bf38fa1ba54703fbe140
리비 <Leekh10195@gmail.com>
2025-02-08 05:06:46 +0900

    [Fix] - 서로 다른 테이블에 중복 저장되는 여행기 좋아요 정보 궁극적 일관성 확보 (#651)
    * fix: Repository 테스트에서 DataJpaTest로 인해 실 서비스와 다른 트랜잭션이 생성되지 않도록 수정
    
    * feat: 실제 좋아요 정보와 여행기에 저장된 좋아요 개수를 동기화 하는 기능 추가
    
    * feat: 매주 월요일 오전 3시 좋아요 개수를 동기화하는 배치 작업이 실행되도록 스케줄러 등록
    
    * feat: ScheduleConfig에서 스케줄링 Enable

--- backend/src/main/java/kr/touroot/travelogue/repository/TravelogueRepository.java
+++ backend/src/main/java/kr/touroot/travelogue/repository/TravelogueRepository.java
@@ -5,8 +5,19 @@ import kr.touroot.travelogue.domain.Travelogue;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface TravelogueRepository extends JpaRepository<Travelogue, Long> {
 
     Page<Travelogue> findAllByAuthor(Member author, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("""
+             UPDATE Travelogue t
+             SET t.likeCount = (SELECT COUNT(*) FROM TravelogueLike l WHERE l.travelogue.id = t.id)
+            """)
+    void syncLikeCounts();
 }
